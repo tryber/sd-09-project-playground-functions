7fdd38c1c0255a32dca608f2cc02943dcae459e6
ViniciusChagas <viniciushrcs@gmail.com>
2020-11-18 16:02:43 -0300

    desafio7

--- src/challenges.js
+++ src/challenges.js
@@ -60,8 +60,18 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distancia1 = Math.abs(cat1 - mouse)
+  let distancia2 = Math.abs(cat2 - mouse)
+
+  if (distancia1 < distancia2){
+    return 'cat1'
+  } else if (distancia1 > distancia2) {
+    return 'cat2'
+  } else if (distancia1 === distancia2) {
+    return 'os gatos trombam e o rato foge'
+  }
+  
 }
 
 // Desafio 8
