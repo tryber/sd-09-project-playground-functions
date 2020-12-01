5d6ffa8de99725173c94b2c117b1cf468b82fc51
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2020-12-01 14:26:04 -0400

    Faz desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,11 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(param1, param2) {
   // seu código aqui
+  if ((param1 == true) && (param2 == true)){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
