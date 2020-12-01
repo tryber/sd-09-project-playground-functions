9e60f7824d3f1d913a3616984897dd8f659252e0
Ederson S <edersonrodrigo31@gmail.com>
2020-12-01 18:24:21 -0300

    fix decimo-primeiro desafio

--- src/challenges.js
+++ src/challenges.js
@@ -190,7 +190,7 @@ function generatePhoneNumber(numbearray) {
       numberphone += numbearray[index] + ") "
     }
     else if (index == 6) {
-      numberphone += numbearray[index] + " - "
+      numberphone += numbearray[index] + "-"
     }
     else {
       numberphone += numbearray[index]
@@ -220,7 +220,7 @@ function maxrepeat(numbers) {
   }
 }
 
-console.log(generatePhoneNumber([5, 1, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   if (lineA > (lineB + lineC) || lineA < Math.abs(lineB - lineC)) {
