e8f6fad87cfce1f86b40e708c632a3223488ef99
Rafael Geronimo <rafaelgeronimo@outlook.com>
2020-12-01 17:19:37 -0300

    Desafio 9 - Codifique e Decodifique

--- src/challenges.js
+++ src/challenges.js
@@ -75,11 +75,58 @@ function fizzBuzz(numbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(phrase) {
+  let encoded = "";
+  for(let index = 0; index < phrase.length; index += 1){
+  	switch (phrase[index]){
+    	case 'a':
+      	encoded = encoded.concat('1');
+        break;
+      case 'e':
+      	encoded = encoded.concat('2');
+        break;
+      case 'i':
+      	encoded = encoded.concat('3');
+        break;
+      case 'o':
+      	encoded = encoded.concat('4');
+        break;
+      case 'u':
+      	encoded = encoded.concat('5');
+        break;
+      default: 
+      	encoded = encoded.concat(phrase[index]);
+      	break;
+    }
+  }
+  return encoded;
 }
-function decode() {
-  // seu código aqui
+
+function decode(phrase) {
+	let decoded = "";
+  for(let index = 0; index < phrase.length; index += 1){
+  	switch (phrase[index]){
+    	case '1':
+      	decoded = decoded.concat('a');
+        break;
+      case '2':
+      	decoded = decoded.concat('e');
+        break;
+      case '3':
+      	decoded = decoded.concat('i');
+        break;
+      case '4':
+      	decoded = decoded.concat('o');
+        break;
+      case '5':
+      	decoded = decoded.concat('u');
+        break;
+      default: 
+      	decoded = decoded.concat(phrase[index]);
+      	break;
+    }
+  }
+  return decoded;
 }
 
 // Desafio 10
