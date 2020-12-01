4fdaedd765ddcbb8ecff6ece9d938bf9b44a9df9
SantosDiv <diogenesfelipe.s@outlook.com>
2020-12-01 16:30:20 -0300

    Resolucao do Desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -31,8 +31,20 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arrayNumbers) {
+  let arrayOrdenad = arrayNumbers.sort(compareNumbers);
+  let numberMax = arrayOrdenad.length - 1;
+  let cont = 0;
+  for (let index = 0; index < arrayOrdenad.length; index += 1) {
+    if (arrayOrdenad[index] === arrayOrdenad[numberMax]) {
+      cont += 1;
+    }
+  }
+  return cont;
+}
+
+function compareNumbers(a, b) {
+  return a - b;
 }
 
 // Desafio 7
