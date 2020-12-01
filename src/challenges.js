21062fcb3a1dffeb43008d765eaafcf8545b1808
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-01 16:07:29 -0300

    Chalenge 2 done

--- src/challenges.js
+++ src/challenges.js
@@ -1,18 +1,97 @@
 // Desafio 1
-//JavaScript possui um operador lógico &&, o qual recebe dois valores e retorna true se ambos os valores são verdadeiros, e retorna false se algum dos valores não o for.
+// function compareTrue(value1, value2) {  
+//   if( value1 === true && value2 === true){
+//     console.log(true);
+//   }else{
+//     console.log(false);
+//   }
+//   return console.log;
+// }
+// compareTrue(true, true);
 
-//Considerando isso, crie uma função chamada compareTrue que, ao receber dois booleanos:
+// Desafio 2
+//Escreva uma função com o nome calcArea que receba um valor de base (chamado base) e outro de altura (chamado height) de um triângulo e retorne o cálculo da sua área.
 
-//Retorne true se ambos os valores são verdadeiros;
-//Retorne false se um ou ambos os parâmetros forem falsos.
-//Faça a função utilizando o operador &&.
-
-function compareTrue(value1, value2) {  
-  if( value1 === true && value2 === true){
-    console.log(true)
-  }else{
-    console.log(false)
-  }
-  return console.log
+//Lembre-se que a área de um triângulo é calculada através da seguinte fórmula: (base * altura) / 2.
+function calcArea(base, height) {
+  let triangle = []
+  triangle = ((base * height)/2);
+  return triangle
 }
-compareTrue(true, true)
+console.log(calcArea(10, 5))
+
+// // Desafio 3
+// function splitSentence() {
+//   // seu código aqui
+// }
+
+// // Desafio 4
+// function concatName() {
+//   // seu código aqui
+// }
+
+// // Desafio 5
+// function footballPoints() {
+//   // seu código aqui
+// }
+
+// // Desafio 6
+// function highestCount() {
+//   // seu código aqui
+// }
+
+// // Desafio 7
+// function catAndMouse() {
+//   // seu código aqui
+// }
+
+// // Desafio 8
+// function fizzBuzz() {
+//   // seu código aqui
+// }
+
+// // Desafio 9
+// function encode() {
+//   // seu código aqui
+// }
+// function decode() {
+//   // seu código aqui
+// }
+
+// // Desafio 10
+// function techList() {
+//   // seu código aqui
+// }
+
+// // Desafio 11
+// function generatePhoneNumber() {
+//   // seu código aqui
+// }
+
+// // Desafio 12
+// function triangleCheck() {
+//   // seu código aqui
+// }
+
+// // Desafio 13
+// function hydrate() {
+//   // seu código aqui
+// }
+
+
+// module.exports = {
+//   calcArea,
+//   catAndMouse,
+//   compareTrue,
+//   concatName,
+//   decode,
+//   encode,
+//   fizzBuzz,
+//   footballPoints,
+//   generatePhoneNumber,
+//   techList,
+//   highestCount,
+//   hydrate,
+//   splitSentence,
+//   triangleCheck,
+// }
