df5189740f6cf26e1a399d6f4e10277db2191064
Rafa Reis <rafaelrnascimento2006@gmail.com>
2020-12-01 17:12:07 -0300

    requisito 4 feito

--- src/challenges.js
+++ src/challenges.js
@@ -19,7 +19,8 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName() {
-  // seu código aqui
+  let newArray = [array[0] , array[array.length -1]];
+  return newArray;
 }
 
 // Desafio 5
