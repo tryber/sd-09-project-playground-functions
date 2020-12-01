d0ec6f44c5f613b852b51d4369e0b11e9a73bfa6
Matheus Schreiner <matheusrubisch@gmail.com>
2020-12-01 18:53:26 -0300

    Function 8

--- src/challenges.js
+++ src/challenges.js
@@ -1,4 +1,4 @@
-// Desafio 1
+// Desafio 1 OK
 function compareTrue(value1, value2) {
   // seu código aqui
   if (value1 === true && value2 === true) {
@@ -7,14 +7,14 @@ function compareTrue(value1, value2) {
   return false;
 }
 
-// Desafio 2
+// Desafio 2 OK
 function calcArea(base, height) {
   // seu código aqui
   let area = ((base * height) / 2);
   return area;
 }
 
-// Desafio 3
+// Desafio 3 OK
 function splitSentence(phrase) {
   // seu código aqui
   let phraseString = phrase;
@@ -32,9 +32,9 @@ function concatName(array) {
   return concat;
 }
 
-console.log (concatName('pedro, joao, johnatas, bernardo, issac'));
+console.log(concatName('pedro, joao, johnatas, bernardo, issac'));
 
-// Desafio 5
+// Desafio 5 OK
 function footballPoints(wins, ties) {
   // seu código aqui
   let totalNumber = ((wins * 3) + ties);
@@ -42,7 +42,7 @@ function footballPoints(wins, ties) {
   return totalNumber;
 }
 
-// Desafio 6
+// Desafio 6 OK (condeclimate)
 function highestCount(arrayNumber) {
   // seu código aqui
   let bigNumber = arrayNumber[0];
@@ -54,7 +54,6 @@ function highestCount(arrayNumber) {
       bigNumber = sweepNumber;
     }
   }
-  
   for (let count = 0; count < arrayNumber.length; count += 1) {
     if (bigNumber === arrayNumber[count]) {
       result += 1;
@@ -63,9 +62,9 @@ function highestCount(arrayNumber) {
   return result;
 }
 
-//console.log (highestCount([1,9,7,6,8,9,3,5,9,8,6,9]));
+console.log (highestCount([1,9,7,6,8,9,3,5,9,8,6,9]));
 
-// Desafio 7
+// Desafio 7 OK (codeclimate)
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
   let count2 = cat2;
@@ -81,20 +80,67 @@ function catAndMouse(mouse, cat1, cat2) {
   }
 }
 
-
-
 // Desafio 8
-function fizzBuzz(numbers) {
+function fizzBuzz(array) {
   // seu código aqui
+  let result = [];
+  for (let count = 0; count < array.length; count += 1) {
+    if ((array[count] % 3) === 0 && (array[count] % 5) === 0) {
+      result.push('fizzBuzz');
+    } else if ((array[count] % 3) === 0 && (array[count] % 5) !== 0) {
+      result.push('fizz');
+    } else if ((array[count] % 3) !== 0 && (array[count] % 5) === 0) {
+      result.push('buzz');
+    } else if ((array[count] % 3) !== 0 && (array[count] % 5) !== 0) {
+      result.push('bug!');
+    }
+  }
+  return result;
 }
+
+console.log(fizzBuzz([1,9,15,7,30]));
 
 // Desafio 9
-function encode() {
+function encode(texto) {
   // seu código aqui
+  let transLettersNumbers = texto;
+  for (let count = 0; count < transLettersNumbers.length; count += 1) {
+    if (transLettersNumbers[count] === 'a') {
+      transLettersNumbers[count] = 1;
+    } else if (transLettersNumbers[count] === 'e') {
+      transLettersNumbers[count] = 2;
+    } else if (transLettersNumbers[count] === 'i') {
+      transLettersNumbers[count] = 3;
+    } else if (transLettersNumbers[count] === 'o') {
+      transLettersNumbers[count] = 4;
+    } else if (transLettersNumbers[count] === 'u') {
+      transLettersNumbers[count] = 5;
+    }
+  }
+  return transLettersNumbers;
 }
-function decode() {
+
+console.log(encode('um texto aqui para teste'));
+
+function decode(transLettersNumbers) {
   // seu código aqui
+  for (let count = 0; count < transLettersNumbers.length; count += 1) {
+    if (transLettersNumbers[count] === 1) {
+      transLettersNumbers[count] = 'a';
+    } else if (transLettersNumbers[count] === 2) {
+      transLettersNumbers[count] = 'e';
+    } else if (transLettersNumbers[count] === 3) {
+      transLettersNumbers[count] = 'i';
+    } else if (transLettersNumbers[count] === 4) {
+      transLettersNumbers[count] = 'o';
+    } else if (transLettersNumbers[count] === 5) {
+      transLettersNumbers[count] = 'u';
+    }
+  }
+  return transLettersNumbers;
 }
+
+console.log(decode('1t 2hfk2 kfl3 jfdklg5'));
 
 // Desafio 10
 function techList() {
