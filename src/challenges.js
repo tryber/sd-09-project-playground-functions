780e19452ebc3346e9e85a2c9cc3c236669baeb7
Luciano_Lodi <lrlodi@gmail.com>
2020-12-01 17:31:45 -0300

    Correçoes de CC

--- src/challenges.js
+++ src/challenges.js
@@ -1,26 +1,26 @@
-//#VQV - LRLODI
+// #VQV - LRLODI
 // Desafio 1
-function compareTrue(value1, value2) {
-if (value1, value2 === true){
-  return true;
-}
-return false;
+function compareTrue( value1, value2 ) {
+  if ( value1=== true && value2 === true ) {
+    return true;
+  }
+  return false;
 }
 
 // Desafio 2
-function calcArea(base, height) {
-  let area = (base * height)/2;
+function calcArea( base, height ) {
+  let area = ( base * height )/2;
   return area;
 }
 
 // Desafio 3
-function splitSentence(string) {
+function splitSentence( string ) {
   let finalArray = [];
   let tempArray = '';
 
-  for(let index = 0 ; index < string.length ; index += 1){
-    tempArray = tempArray + string[index];
-    if(string[index + 1] == ' ' || index == string.length - 1){
+  for ( let index = 0; index < string.length; index += 1 ) {
+    tempArray += string[index];
+    if ( string[index + 1] === ' ' || index === string.length - 1 ) {
       finalArray.push(tempArray);
       tempArray = '';
     }
@@ -30,14 +30,14 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(array) {
-  let finalArray = array[0] + ' ' + array[array.length - 1];
+  let finalArray = array[0] + ' ' + ${array[array.length - 1]};
   return finalArray;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   let totalPoints;
-  totalPoints = (wins * 3) + (ties);
+  totalPoints = ( wins * 3  ) + (ties);
   return totalPoints;
 }
 
