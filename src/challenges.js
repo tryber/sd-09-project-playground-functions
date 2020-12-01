67f1fccad21f7bb963e1ea957d94f2e17ad1808a
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-01 15:55:57 -0300

    Chalenge 1 done

--- src/challenges.js
+++ src/challenges.js
@@ -1,85 +1,18 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-}
+//JavaScript possui um operador lógico &&, o qual recebe dois valores e retorna true se ambos os valores são verdadeiros, e retorna false se algum dos valores não o for.
 
-// Desafio 2
-function calcArea() {
-  // seu código aqui
-}
+//Considerando isso, crie uma função chamada compareTrue que, ao receber dois booleanos:
 
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
-}
+//Retorne true se ambos os valores são verdadeiros;
+//Retorne false se um ou ambos os parâmetros forem falsos.
+//Faça a função utilizando o operador &&.
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
+function compareTrue(value1, value2) {  
+  if( value1 === true && value2 === true){
+    console.log(true)
+  }else{
+    console.log(false)
+  }
+  return console.log
 }
-
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
-
-// Desafio 6
-function highestCount() {
-  // seu código aqui
-}
-
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
-
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
-
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
-
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
-
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
-
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
-
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
-
-
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  generatePhoneNumber,
-  techList,
-  highestCount,
-  hydrate,
-  splitSentence,
-  triangleCheck,
-}
+compareTrue(true, true)
