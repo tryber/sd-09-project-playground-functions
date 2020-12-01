9e68e826110f4ec3d8a88836e23b7d7679fe9f31
Igor Marcos Riegel <igor.riegel@gmail.com>
2020-12-01 15:29:00 -0300

    Resolucao do desfio 2 - calcArea.

--- src/challenges.js
+++ src/challenges.js
@@ -2,7 +2,7 @@
 
 // Desafio 1
 function compareTrue(bool1, bool2) {
-  if (bool1 && bool2){
+  if (bool1 && bool2) {
     return true;
   } else {
     return false;
@@ -10,8 +10,9 @@ function compareTrue(bool1, bool2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
