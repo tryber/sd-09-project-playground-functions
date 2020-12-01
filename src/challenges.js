dbb9b5bee00247356da6cae01d459cdddceab32c
PeryclesF <perycles_reis@hotmail.com>
2020-12-01 18:47:42 -0300

    enviando desafio 3 corrigindo erro 01

--- src/challenges.js
+++ src/challenges.js
@@ -19,7 +19,7 @@ console.log(`A área do triângulo é: ${calcArea(10, 0)}`);
 
 // Desafio 3
 function splitSentence(string) {
-  novaString = string.split(' ');
+  let novaString = string.split(' ');
   return novaString;
 }
 let string = 'go Trybe';
