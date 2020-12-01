4fc17935fb6e9819ae882af1459d403de75fa819
Pamela Anderson Sales <pamelasalesr@gmail.com>
2020-12-01 16:10:55 -0300

    Exercise 1 - first try

--- src/challenges.js
+++ src/challenges.js
@@ -6,11 +6,9 @@ function compareTrue(value1, value2) {
   value1 = true;
   value2 = true;
 
-  if(value1 === true && value2 === true) {
+  if (value1 === true && value2 === true) {
     return true;
-  } else {
-    return false;
-  }
+  } return false;
 }
 
 // Desafio 2
