a5190a8747ceb1513766beaa4a84a66eaddcd8e3
Daniela Muller <danielamuller20@gmail.com>
2020-12-01 17:13:31 -0300

    Projeto JS

--- src/challenges.js
+++ src/challenges.js
@@ -2,7 +2,7 @@
 function compareTrue(boolean1, boolean2) {
   if (boolean1 === true && boolean2 === true) {
     return true;
-  } 
+  }
   return false;
 }
 console.log(compareTrue(false, true))
@@ -22,9 +22,16 @@ function splitSentence(string) {
 console.log(splitSentence('Vamos nessa'))
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let firstName = [];
+  let lastName = [];
+  for (let index = 0; index <= array.length; index += 1) {
+    firstName = array[0];
+    lastName = array[index - 1];
+  }
+  return firstName + ', ' + lastName;
 }
+console.log(concatName(['Roberta', 'Julia', 'João', 'Maria', 'Marcos']))
 
 // Desafio 5
 function footballPoints() {
