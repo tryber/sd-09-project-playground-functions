6cec169c468fb442075ed64d1be4960b34696a75
ggaldino95 <ggaldino95@gmail.com>
2020-12-01 17:00:37 -0300

    [Task 4] Created function concatName to concatenate the last and the first string in an Array

--- src/challenges.js
+++ src/challenges.js
@@ -30,8 +30,8 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arrayString) {
+  return arrayString[arrayString.length - 1] + ", " + arrayString[0];
 }
 
 // Desafio 5
