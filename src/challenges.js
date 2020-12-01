c62de60583509399a982eceefad803205ab88661
ggaldino95 <ggaldino95@gmail.com>
2020-12-01 17:53:29 -0300

    [Task 7] Small function fix

--- src/challenges.js
+++ src/challenges.js
@@ -58,7 +58,11 @@ function highestCount(array) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if (cat1 === cat2) {
+  cat1 = Math.abs(cat1);
+  cat2 = Math.abs(cat2);
+  mouse = Math.abs(mouse);
+
+  if ((cat1 - mouse) === (cat2 - mouse)) {
     return 'os gatos trombam e o rato foge'
   } else if ((cat1 - mouse) > (cat2 - mouse)) {
     return 'cat1';
