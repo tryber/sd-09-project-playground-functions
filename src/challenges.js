bc151fe51c08c9d085fde4fba18347492bd805ce
LeoMajevski <leonardo.majevski@gmail.com>
2020-12-01 15:49:29 -0300

    2. Dividindo a frase

--- src/challenges.js
+++ src/challenges.js
@@ -15,8 +15,11 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let string = 'go Trybe';
+  string.split(',')
+
+  return string;
 }
 
 // Desafio 4
