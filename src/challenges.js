8162393700ae5523db7a263e6d353de1b878ab75
Natalia Negreiros <nataliaknegres@gmail.com>
2020-12-01 15:57:03 -0300

    Adiciona exercicio 5

--- src/challenges.js
+++ src/challenges.js
@@ -29,9 +29,10 @@ function concatName(param) {
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
+console.log(footballPoints(3, 1))
 
 // Desafio 6
 function highestCount() {
