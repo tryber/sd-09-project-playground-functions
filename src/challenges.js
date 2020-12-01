bfff439fea787cff6b95f2ee386b638327a623aa
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2020-12-01 15:32:56 -0300

    desafio de calculo da area de um triangulo corrigido

--- src/challenges.js
+++ src/challenges.js
@@ -7,8 +7,10 @@ function compareTrue(value1, value2) {
 }
 
 // Desafio 2
-function calcArea(base, height, area) {
+function calcArea(base, height) {
+  let area = 0;
   area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
