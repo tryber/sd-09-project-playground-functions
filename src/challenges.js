f96a33d58380a4fbe632845e2db9c5dd53aa072d
Diego Vini <analistamm@gmail.com>
2020-12-01 18:10:01 -0300

    Retorna array

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let result = string.split(" ");
+  return result;
 }
 
 // Desafio 4
