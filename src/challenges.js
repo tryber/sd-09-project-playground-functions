b01b78a21bedb42d1c3ab3ba477d0a209672f83e
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2020-12-01 17:12:39 -0300

    Exercicio 6 Concluido

--- src/challenges.js
+++ src/challenges.js
@@ -25,51 +25,65 @@ function concatName(a) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(nums) {
+  let maior = nums[0];
+  for(let i = 0; i < nums.length; i += 1){
+    if(nums[i] > maior){
+        maior = nums[i]
+    } else{
+    }
+  }  
+  for(let i = 0; i < nums.length; i += 1){
+    let contagem = 0
+    if(nums[i] == maior){
+        contagem += 1
+    } else{
+    }
+  }
 }
+return contagem
 
 // Desafio 7
 function catAndMouse() {
-  // seu código aqui
+  
 }
 
 // Desafio 8
 function fizzBuzz() {
-  // seu código aqui
+  
 }
 
 // Desafio 9
 function encode() {
-  // seu código aqui
+  
 }
 function decode() {
-  // seu código aqui
+  
 }
 
 // Desafio 10
 function techList() {
-  // seu código aqui
+  
 }
 
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
+  
 }
 
 // Desafio 12
 function triangleCheck() {
-  // seu código aqui
+  
 }
 
 // Desafio 13
 function hydrate() {
-  // seu código aqui
+  
 }
 
 
