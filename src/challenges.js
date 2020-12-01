f878ba7696756c1ad8f284763871c40e551401aa
alailsonko <aalailson3@gmail.com>
2020-12-01 15:47:35 -0300

    question 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,9 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-      if (value1 === true && value2 === true){
-        return true;
-      }
-      if (value1 === false && value2 === false){
-        return false
-      }
+  if (value1 === true && value2 === true){
+    return true;
+  }
+ return false
 }
 
 // Desafio 2
