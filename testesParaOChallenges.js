041860280fdc84eb7afe8403d66a055047b1cb37
DaniloEstevam <danilo.aaab@gmail.com>
2020-12-01 19:04:46 -0300

    exercicio 3 concluido

--- testesParaOChallenges.js
+++ testesParaOChallenges.js
@@ -1,5 +1,16 @@
-function footballPoints(wins, ties) {
-   pontos = wins * 3 + ties 
-   return pontos
-  } 
-console.log(footballPoints(5,2))
+// EX 5 COMENTADO
+// function footballPoints(wins, ties) {
+//    pontos = wins * 3 + ties 
+//    return pontos
+//   } 
+// console.log(footballPoints(5,2))
+
+
+// Desafio 3
+let array = 'go trybe'
+function splitSentence(string) {
+   let divisor = string.split(' ');
+   console.log(divisor);
+   return string;
+  }
+  console.log(splitSentence(array));
