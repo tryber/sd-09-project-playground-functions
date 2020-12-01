177a0da2a72a25af2d44ad958c6b4449508a59be
Luciano_Lodi <lrlodi@gmail.com>
2020-12-01 15:17:33 -0300

    Commit inicial - Requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
-function compareTrue(param1 , param2) {
-  if (param1 === true && param2 === true){
+function compareTrue(bool1 , bool2) {
+  if (bool1 == true && bool2 == true){
     return true;
   } else {
     return false;
