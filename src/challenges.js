982dca6438bfff72d4bb55f91cd9a30362af9b53
Henrique Brito Elias <henriquebelias@gmail.com>
2020-12-01 18:21:01 -0300

    Requisito 9 - Codifique e Decodifique

--- src/challenges.js
+++ src/challenges.js
@@ -69,11 +69,16 @@ function fizzBuzz(numbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(phrase) {
+  let coded = phrase.replace(/a/g, 1).replace(/e/g, 2).replace(/i/g, 3).replace(/o/g, 4).replace(/u/g, 5);
+  
+  return coded;    
 }
-function decode() {
-  // seu código aqui
+
+function decode(phrase) {
+  let decoded = phrase.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o').replace(/5/g, 'u');
+
+  return decoded;
 }
 
 // Desafio 10
