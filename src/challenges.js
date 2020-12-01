fc89e361fdf29cbe2fdf07897deca4a7b8759b69
Rafa Reis <rafaelrnascimento2006@gmail.com>
2020-12-01 18:56:56 -0300

    corrigindo issues apontadas pelo codeclimate

--- src/challenges.js
+++ src/challenges.js
@@ -20,7 +20,7 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(array) {
-  return array[0] + ", " + array[array.length - 1];
+  return `${array[array.length - 1]}, ${array[0]}`;
 }
 
 // Desafio 5
