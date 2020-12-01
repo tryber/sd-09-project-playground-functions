b3a329d97b1f5286ee47c6d155d8cc0329b0e5b1
alailsonko <aalailson3@gmail.com>
2020-12-01 17:31:20 -0300

    fix(linter): indentation challenge 8

--- src/challenges.js
+++ src/challenges.js
@@ -55,8 +55,8 @@ function catAndMouse(mouse, cat1, cat2) {
 
 function selectFizzBuzz(value) {
   if (value % 3 === 0) {
-      return 'fizz'
-    } else if (value % 5 === 0) {
+    return 'fizz'
+  } else if (value % 5 === 0) {
       return 'buzz'
     } else if (value % 3 === 0 && value % 5 === 0) {
       return 'fizzBuzz'
