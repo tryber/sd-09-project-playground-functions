1bc3dd3ee185047e3ff4e07670fc939a79cfb9a2
Renan Costa <renancosta21@gmail.com>
2020-12-01 17:24:14 -0300

    requisito 3

--- challenges.js
+++ challenges.js
@@ -1,7 +1,7 @@
 function compareTrue(bool1, bool2) {
   bool1 = true;
   bool2 = true;
-  if ((bool1 === true) && (bool2 === true)) {
+  if (bool1 === true && bool2 === true) {
     return true;
   }
   return false;
@@ -16,7 +16,7 @@ function calcArea(base, height) {
 calcArea();
 
 function splitSentence(nomeCompleto) {
-    nomeCompleto = 'Renan de Oliveira Costa';
-    return nomeCompleto.split([' ']);
+  nomeCompleto = 'Renan de Oliveira Costa';
+  return nomeCompleto.split([' ']);
 }
-console.log(splitSentence());
+splitSentence();
