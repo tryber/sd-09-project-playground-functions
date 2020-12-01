3e825f6f2bac5d94e4937c3ed6e759131c732a37
fernandoresende <fernandocastellob@gmail.com>
2020-12-01 16:07:12 -0300

    desafio 1 e 2 feitos

--- src/challenges.js
+++ src/challenges.js
@@ -1,12 +1,17 @@
-//Iniciando projeto #VQV!
+
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(value1 , value2) {
+  if(value1 == true && value2 == true){
+    return true
+  }else{
+    return false
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, altura) {
+  let area = (base*altura)/2;
+    return area
 }
 
 // Desafio 3
