0ce369f6747cdc36c8dc3f5a8866c3becc5b048a
Hudson Carlos <hc88806@gmail.com>
2020-12-01 16:25:25 -0400

    funçao 1 e 2 finalizada

---
+++ challenges.js
@@ -0,0 +1,10 @@
+function compareTrue(a,b){
+    if(a===true && b===true){
+        return true;
+    }else{
+        return false;
+    }
+};
+function calcArea(height,base){
+    return (base*height)/2;
+};console.log(calcArea(10,5));
