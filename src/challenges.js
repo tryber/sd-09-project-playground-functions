2ff68e53c04f15b615e2327ee9f83a7e2ddf813a
Tiago Yoneda <xyonedax@gmail.com>
2020-12-01 15:11:49 -0300

    funcao 3

--- src/challenges.js
+++ src/challenges.js
@@ -16,8 +16,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(arraySource) {
+  newArray = arraySource.split(' ');
+  return newArray;
 }
 
 // Desafio 4
