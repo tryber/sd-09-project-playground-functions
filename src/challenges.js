f39ffe2b71092b9366efc268c75764b3ea283ba4
Johne Alves <johne_af@hotmail.com>
2020-12-01 16:21:17 -0300

    adiciona desafio 4

--- src/challenges.js
+++ src/challenges.js
@@ -11,21 +11,39 @@ function compareTrue(value1, value2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  let area = (base * height)/2
-  
+  let area = (base * height) / 2;
   return area;
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(sentence) {
+  let arraySentence = [];
+  for(let index = 0; index < sentence.length; index += 1) {
+    if(sentence[index] !== ' ') {
+      arraySentence.push(sentence[index])
+    }
+  }
   // seu código aqui
+  return arraySentence
 }
+
+console.log(splitSentence('go Trybe'));
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arrayNomes) {
+  let nomes = [];
+  for(let index = arrayNomes.length - 1; index >= 0; index -= 1) {
+    if (arrayNomes[index] === arrayNomes[arrayNomes.length - 1]) {
+      nomes.push(arrayNomes[index])
+    } else if (arrayNomes[index] === arrayNomes[0]) {
+      nomes.push(arrayNomes[index])
+    }
+  }
+  
+  return nomes;
 }
 
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
 // Desafio 5
 function footballPoints() {
   // seu código aqui
