908709c27b3ca926127e2e4aa405241a3f61841c
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-01 15:40:10 -0300

    Desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -10,8 +10,8 @@ function compareTrue(boolean1, boolean2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return base*height/2
 }
 
 // Desafio 3
