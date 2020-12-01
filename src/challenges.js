02a3405c8df27be6f45e14f03d3f57728227b9d5
Raphael Rêgo <raphael.mlrego@gmail.com>
2020-12-01 16:40:07 -0300

    desafio 5 e 6

--- src/challenges.js
+++ src/challenges.js
@@ -18,20 +18,32 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(arrayNames) {
-  let phrase = arrayNames[arrayNames.length -1] + ', ' + arrayNames[0];
+  let phrase = arrayNames[arrayNames.length - 1] + ', ' + arrayNames[0];
   return phrase;
 }
-console.log(concatName(['raphael', 'abdoraldo', 'felipe', 'rogerio']));
+
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (wins * 3) + ties;
+  return points;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arrayNumbers) {
+  let counter = 0;
+  let highestNumber = 0;
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    if (highestNumber < arrayNumbers[index]) {
+      highestNumber = arrayNumbers[index];
+      counter = 0;
+    }
+    if (arrayNumbers[index] === highestNumber) {
+      counter += 1;
+    }
+  }
+  return counter;
 }
-
+console.log(highestCount([2, 3, 1, 3, 3, 1, 3, 5]));
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
