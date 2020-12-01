75b9d8b1c94c47a476a528e1edd33a1204320f58
Rafa Reis <rafaelrnascimento2006@gmail.com>
2020-12-01 17:34:09 -0300

    corrigindo issues apontadas pelo codeclimate

--- src/challenges.js
+++ src/challenges.js
@@ -3,10 +3,9 @@
 // Desafio 1
 function compareTrue(value1, value2) {
   if ((value1 === true) && (value2 === true)) {
-    return ("true");
-  } else {
-    return ("false");
+    return true;
   }
+  return false;
 }
 
 // Desafio 2
