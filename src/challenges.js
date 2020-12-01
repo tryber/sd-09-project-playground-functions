7d1719582614b75dd52ed3f9f68619a814f542c8
BBrolesi <brunohsbrolesi@gmail.com>
2020-12-01 18:48:33 -0300

    Add challenge 11

--- src/challenges.js
+++ src/challenges.js
@@ -203,8 +203,6 @@ function swapVowelNumber(vowel) {
     return ('(' + numberArray.slice(0, 2) + ')' + numberArray.slice(2, 7) + '-' + numberArray.slice(7, 11));
   }
 
-  console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
-
   // Desafio 12
   function triangleCheck() {
     // seu código aqui
