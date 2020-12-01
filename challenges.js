f664ade296f25fb94db340d9b02ebd1691ba8ca7
Renan Costa <renancosta21@gmail.com>
2020-12-01 16:09:03 -0300

    requisito 1

--- challenges.js
+++ challenges.js
@@ -1,5 +1,11 @@
 function compareTrue(bool1, bool2) {
+  bool1 = true;
+  bool2 = false;  
   if ((bool1 === true) && (bool2 === true)) {
     return true;
   }
+  else {
+    return false;
+  }
 }
+console.log(compareTrue());
