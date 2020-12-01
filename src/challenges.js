deeebc2768719fd29643988757721a1cb7752ebd
Matheus Monteiro <monteiromsh@gmail.com>
2020-12-01 16:47:25 -0300

    primeiros testes correcao

--- src/challenges.js
+++ src/challenges.js
@@ -7,21 +7,21 @@ function compareTrue(boolean1, boolean2) {
   } else {
     return false;
   }
-} 
+} compareTrue(true,true)
 
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
   let area = (base*height)/2;
   return area;
-}
+} calcArea(4,8)
 
 // Desafio 3
 function splitSentence(frase) {
   // seu código aqui
 let arrayFrase = frase.split(" ")
 return arrayFrase
-}
+} splitSentence('go Trybe')
 
 // Desafio 4
 function concatName() {
