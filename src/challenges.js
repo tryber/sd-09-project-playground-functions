c48d06d880c3e5db7ff36e2787252407f030829d
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-01 17:39:22 -0300

    first commit check at github

--- src/challenges.js
+++ src/challenges.js
@@ -1,51 +1,47 @@
 // Desafio 1
-// function compareTrue(value1, value2) {  
-//   if( value1 === true && value2 === true){
-//     console.log(true);
-//   }else{
-//     console.log(false);
-//   }
-//   return console.log;
-// }
-// compareTrue(true, true);
+function compareTrue(value1, value2) {
+  if( value1 === true && value2 === true){
+    console.log(true);
+  }else{
+    console.log(false);
+  }
+  return console.log;
+}
+compareTrue(true, true);
 
 // Desafio 2
 
-// function calcArea(base, height) {
-//   let triangle = []
-//   triangle = ((base * height)/2);
-//   return triangle
-// }
-// console.log(calcArea(10, 5))
+function calcArea(base, height) {
+  let triangle = []
+  triangle = ((base * height)/2);
+  return triangle
+}
+console.log(calcArea(10, 5));
 
-// // Desafio 3
-// function splitSentence(splitSentence) {
-//   let string = splitSentence
-//   return console.log(string.split(' '))
-// }
-// splitSentence('go Trybe')
+// Desafio 3
+function splitSentence(splitSentence) {
+  let string = splitSentence;
+  return console.log(string.split(' '));
+}
+splitSentence('go Trybe');
 
-// // Desafio 4
+// Desafio 4
 
-// function concatName(arraySTR) {
-//   let string = arraySTR.reverse(' ')
-//   return console.log(string)
-// }
-// concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'])
+function concatName(arraySTR) {
+  let string = arraySTR.reverse(' ');
+  return console.log(string);
+}
+concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
 
-// // Desafio 5
-// Escreva uma função com o nome footballPoints que receba o número de vitórias (esse parâmetro deverá se chamar wins) e o número de empates (esse parâmetro deverá se chamar ties) e retorne a quantidade de pontos que o time marcou em um campeonato.
+// Desafio 5
 
-// Para tanto, considere que cada vitória vale 3 pontos e cada empate vale 1 ponto.
 function footballPoints(wins, ties) {
   let totalPoints = (wins*3) + ties;
-  return console.log(totalPoints)
+  return console.log(totalPoints);
 }
-footballPoints(10, 5)
+footballPoints(10, 5);
 
-
-
-// // Desafio 6
+// Desafio 6
 // function highestCount() {
 //   // seu código aqui
 // }
