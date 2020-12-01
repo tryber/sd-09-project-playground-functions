26640ce2aff8d27e37ca7fdde70952c0841cbfcb
Luciano_Lodi <lrlodi@gmail.com>
2020-12-01 15:28:48 -0300

    Requisito 2 - Funcao calcArea

--- src/challenges.js
+++ src/challenges.js
@@ -6,11 +6,10 @@ function compareTrue(bool1 , bool2) {
     return false;
   }
 
-
-
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base , height) {
+  let area = (base * height)/2;
+  return area;
 }
 
 // Desafio 3
