cb29714b64b7538d53c0646cd3f098f76861a204
BBrolesi <brunohsbrolesi@gmail.com>
2020-12-01 15:19:28 -0300

    Add challenge 5

--- src/challenges.js
+++ src/challenges.js
@@ -21,8 +21,8 @@ function concatName(strings) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
