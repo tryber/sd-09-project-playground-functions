850636a916ba39beed6e2af20edafda0f41a3b43
danielfasanaro <danielfasanaro@gmail.com>
2020-12-01 16:40:20 -0300

    Requisito 1 - Implementada funcao compareTrue

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,10 @@
 // Daniel
 // Desafio 1
-function compareTrue() {
+function compareTrue(bool1, bool2) {
   // seu código aqui
+  if(bool1 && bool2) {
+    console.log("true");
+  }
 }
 
 // Desafio 2
