e5ff2ddb8856582bd8a44ea814b65330c3e6064e
Rafael Geronimo <rafaelgeronimo@outlook.com>
2020-12-01 15:20:48 -0300

    Desafio 3 - Dividindo a frase

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase) {
+  let words = [];
+  return words = phrase.split(' ');
 }
 
 // Desafio 4
