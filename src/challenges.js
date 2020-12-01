a80811e8ff25debaac6e79bfa2d907939dd376cc
Tiago Yoneda <xyonedax@gmail.com>
2020-12-01 16:26:58 -0300

    primeira revisao do codeclimate

--- src/challenges.js
+++ src/challenges.js
@@ -7,12 +7,12 @@ https://github.com/tiago-yoneda
 
 // Desafio 1
 function compareTrue(bool1, bool2) {
-  return ( bool1 && bool2);
+  return (bool1 && bool2);
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  return base*height/2;
+  return (base * height * 0.5);
 }
 
 // Desafio 3
@@ -24,20 +24,20 @@ function splitSentence(arraySource) {
 // Desafio 4
 function concatName(arraySource) {
   let newArray = [];
-  newArray.push(arraySource[arraySource.length-1],arraySource[0]);
+  newArray.push(arraySource[arraySource.length - 1], arraySource[0]);
   return newArray;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return (3*wins+ties);
+  return (3 * wins + ties);
 }
 
 // funcao auxiliar para 6
-function findHigher(array){
+function findHigher(array) {
   let auxNum = 0;
-  for ( let item of array){
-    if (item >= auxNum){
+  for (let item of array) {
+    if (item >= auxNum) {
       auxNum = item;
     }
   }
@@ -48,9 +48,9 @@ function findHigher(array){
 function highestCount(array) {
   let highest = findHigher(array);
   let counter = 0;
-  for ( let item of array){
-    if (highest === item){
-      counter +=1;
+  for (let item of array) {
+    if (highest === item) {
+      counter += 1;
     }
   }
   return counter;
@@ -58,34 +58,32 @@ function highestCount(array) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let dist1 = Math.abs(mouse-cat1);
-  let dist2 = Math.abs(mouse-cat2);
+  let dist1 = Math.abs(mouse - cat1);
+  let dist2 = Math.abs(mouse - cat2);
 
-  if ( dist1 === dist2){
+  if (dist1 === dist2) {
     return 'os gatos trombam e o rato foge';
   }
-  else if (dist1 < dist2){
+  else if (dist1 < dist2) {
     return 'cat1';
   }
-  else {
-    return 'cat2';
-  }
+  return 'cat2';
 }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(array) {
   let newArray = []
-  for ( let item of array){
-    if ( item%15 == 0){
+  for (let item of array) {
+    if (item%15 === 0) {
       newArray.push('fizzBuzz');
     }
-    else if ( item%3 == 0){
+    else if (item%3 === 0){ 
       newArray.push('fizz');
     }
-    else if ( item%5 == 0){
+    else if (item%5 === 0) {
       newArray.push('Buzz');
     }
-    else{
+    else {
       newArray.push('bug!');
     }
   }
@@ -94,29 +92,29 @@ function fizzBuzz() {
 
 // objeto para ajudar o desafio 9
 const correspondencia = {
-  a:1,
-  e:2,
-  i:3,
-  o:4,
-  u:5
+  a: 1,
+  e: 2,
+  i: 3,
+  o: 4,
+  u: 5,
 }
+
 // funcao auxiliar replaceAll
-function replaceAll(string, busca, troca){
-  newString = string.split(busca).join(troca);
+function replaceAll(string, busca, troca) {
+  let newString = string.split(busca).join(troca);
   return newString;
 }
 
 // Desafio 9
 function encode(string) {
-  
-  for (let item in correspondencia){
+  for (let item in correspondencia) {
     string = replaceAll(string, item, correspondencia[item]);
   }
   return string;
 }
 
 function decode(string) {
-  for (let item in correspondencia){
+  for (let item in correspondencia) {
     string = replaceAll(string, correspondencia[item], item);
   }
   return string;
@@ -124,17 +122,16 @@ function decode(string) {
 
 // Desafio 10
 function techList(array, name) {
-
   array.sort();
   let newArray = [];
   let newObject = {};
 
-  for (let item of array){
+  for (let item of array) {
     newObject.tech = item;
     newObject.name = name;
     newArray.push(newObject);
     newObject = {};
-    }
+  }
   return newArray;
 }
 
