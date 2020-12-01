55e78e823416e10d2367cb4574ca6ecbfb90f2c9
Micaela Alves <micaela.alves93@gmail.com>
2020-12-01 16:39:35 -0300

    Requisito 1 - correçoes

--- src/challenges.js
+++ src/challenges.js
@@ -1,13 +1,12 @@
 // Project Playground Functions
 // Desafio 1
 function compareTrue(value1, value2) {
-  if(value1 === 'true' && value2 === 'true'){
-    return true;
-  }else{
-    return false;
-  }  
+  if (value1 === 'true' && value2 === 'true') {
+    return 'true';
+  } else {
+    return 'false';
+  }
 }
-console.log(compareTrue('true', 'true'));
 
 // Desafio 2
 function calcArea() {
