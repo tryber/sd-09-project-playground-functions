4101330a088b3bfd223129f1d517087276e6c01b
alailsonko <aalailson3@gmail.com>
2020-12-01 16:38:47 -0300

    fix(linter): singlequote and returns

--- src/challenges.js
+++ src/challenges.js
@@ -44,13 +44,13 @@ function highestCount(valueArray) {
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   if ((cat1 - mouse) < (cat2 - mouse)) {
-    return "cat1"
+    return 'cat1'
   } else if ((cat2 - mouse) < (cat1 - mouse)) {
-    return "cat2"
+    return 'cat2'
   } else if ((mouse - cat1) === (mouse - cat2)) {
-    return "os gatos trombam e o rato foge"
+    return 'os gatos trombam e o rato foge'
   }
-  return
+  return 'fails'
 }
 
 // Desafio 8
