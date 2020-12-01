b99d6eb61ac23defaabd74edeb417cb92ed4873a
Caio <caioqs.info@gmail.com>
2020-12-01 20:11:05 -0300

    Feito desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,12 @@
 // Desafio 1
 
-function compareTrue() {
+function compareTrue(value1, value2) {
   // seu código aqui
-  vamos começar
+  if (value1 === true && value2 === true){
+    return true;
+  } else {
+    return false;
+  }
 
 }
 
