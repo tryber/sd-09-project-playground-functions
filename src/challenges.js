e509fabd99db1a73e1e96f1e17f7aab80d8ec4e4
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-01 16:01:33 -0300

    Correcoes no requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,12 +1,10 @@
-//Chama no riks
 // Desafio 1
-function compareTrue() {
+function compareTrue(valor1,valor2) {
   // seu código aqui
   if (valor1 === true && valor2 === true) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 // Desafio 2
