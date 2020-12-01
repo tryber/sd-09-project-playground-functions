463834297ad2a34b4f0b5ef83102d01a89547ab0
Tiago Yoneda <xyonedax@gmail.com>
2020-12-01 19:01:36 -0300

    arrumando desafio 13

--- src/challenges.js
+++ src/challenges.js
@@ -189,15 +189,18 @@ function triangleCheck(lineA, lineB, lineC) {
 
 // Desafio 13
 function hydrate(string) {
-  // extrai todos os digitos com o match / \d /g 
-  //transforma eles de string para numero com map(Number)
+  // extrai todos os digitos com o match / \d /g
+  // transforma eles de string para numero com map(Number)
 
   let numbers = string.match(/\d+/g).map(Number);
   let soma = 0;
   for (let item of numbers) {
     soma += item;
   }
-  return (`${soma} copos de água`);
+  if (soma > 1){
+    return (`${soma} copos de água`);
+  }
+  return (`${soma} copo de água`);
 }
 
 module.exports = {
