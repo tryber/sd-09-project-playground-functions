af9933806b0edbedf90644948761075767a7471a
Andrechiapetta <andreluizchiapetta@gmail.com>
2020-12-01 16:08:12 -0300

    Correção

--- src/challenges.js
+++ src/challenges.js
@@ -20,7 +20,7 @@ function splitSentence() {
 
 // Desafio 4
 function concatName(array) {
-  return array[array.length - 1] + ' , ' + array[0];
+  return `${array[array.length - 1]}, ${array[0]}`;
 }
 
 // Desafio 5
