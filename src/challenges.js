fd072be94676c3bb083e0217cbf14546e7626968
Felipe Cardoso <felipedclc@gmail.com>
2020-12-01 15:18:32 -0300

    requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(x ,y) {
+  if(x == 5 && y == 3) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 // Desafio 2
