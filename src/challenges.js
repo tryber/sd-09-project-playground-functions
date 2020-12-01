1bf315a067150e3d62a3b00e1775780d343df156
J.Henrique <jhenriqueribeirodesa@gmail.com>
2020-12-01 15:51:37 -0300

    1° requisito

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(primeiroParamentro, segundoParamentro) {
+  return primeiroParamentro && segundoParamentro
 }
 
 // Desafio 2
