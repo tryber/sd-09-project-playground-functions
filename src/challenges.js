76c5cf4f2c50e11cbf6b765242948bbb1f46213c
FredericoCanoeira <fcanoeira@gmail.com>
2020-12-02 00:12:24 +0100

    corrigindo requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,13 +1,12 @@
 // Segundo Projecto de Frederico Canoeira #VQV
 // Desafio 1
 function compareTrue(number1, number2) {
-  if (number1 % 2 === 0 && number2 % 2 !== 0) {
+  if (number1 === true && number2 === true) {
     return true
   }
   return false
 }
-//let result = compareTrue(4, 9)
-//console.log(result)
+//console.log(compareTrue(true, true))
 
 // Desafio 2
 function calcArea(base, height) {
@@ -20,80 +19,80 @@ console.log(area)
 let lodo = "Lucas, Marcio, oliveira, Carlos"
 function splitSentence(lodo) {
   //for (let cont = 0; cont <= arr.length; cont += 1) {
-let div = lodo.split(" ");
-console.log(div)
+  let div = lodo.split(" ");
+  console.log(div)
   return lodo
-  }
+}
+//arrayNome[arrayNome.length -1] + "," + arrayName[0]
+// Desafio 4
+function concatName(names) {
+  let concat = names.length
+  return `${names.length[-1]} , ${names[0]}`
 
-  // Desafio 4
-   function concatName(names) {
-   let concat  = names[names.length]
-    return `${names.length[-1]} , ${names[0]}`
+}
 
-  }
+// Desafio 5
+function footballPoints() {
+  // seu código aqui
+}
 
-  // Desafio 5
-  function footballPoints() {
-    // seu código aqui
-  }
+// Desafio 6
+function highestCount() {
+  // seu código aqui
+}
 
-  // Desafio 6
-  function highestCount() {
-    // seu código aqui
-  }
+// Desafio 7
+function catAndMouse() {
+  // seu código aqui
+}
 
-  // Desafio 7
-  function catAndMouse() {
-    // seu código aqui
-  }
+// Desafio 8
+function fizzBuzz() {
+  // seu código aqui
+}
 
-  // Desafio 8
-  function fizzBuzz() {
-    // seu código aqui
-  }
+// Desafio 9
+function encode() {
+  // seu código aqui
+}
+function decode() {
+  // seu código aqui
+}
 
-  // Desafio 9
-  function encode() {
-    // seu código aqui
-  }
-  function decode() {
-    // seu código aqui
-  }
+// Desafio 10
+function techList() {
+  // seu código aqui
+}
 
-  // Desafio 10
-  function techList() {
-    // seu código aqui
-  }
+// Desafio 11
+function generatePhoneNumber() {
+  // seu código aqui
+}
 
-  // Desafio 11
-  function generatePhoneNumber() {
-    // seu código aqui
-  }
+// Desafio 12
+function triangleCheck() {
+  // seu código aqui
+}
 
-  // Desafio 12
-  function triangleCheck() {
-    // seu código aqui
-  }
-
-  // Desafio 13
-  function hydrate() {
-    // seu código aqui
-  }
+// Desafio 13
+function hydrate() {
+  // seu código aqui
+}
 
 
-  module.exports = {
-    calcArea,
-    catAndMouse,
-    compareTrue,
-    concatName,
-    decode,
-    encode,
-    fizzBuzz,
-    footballPoints,
-    generatePhoneNumber,
-    techList,
-    highestCount,
-    hydrate,
-    splitSentence,
-    triangleCheck,
-  }
+module.exports = {
+  calcArea,
+  catAndMouse,
+  compareTrue,
+  concatName,
+  decode,
+  encode,
+  fizzBuzz,
+  footballPoints,
+  generatePhoneNumber,
+  techList,
+  highestCount,
+  hydrate,
+  splitSentence,
+  triangleCheck,
+}
