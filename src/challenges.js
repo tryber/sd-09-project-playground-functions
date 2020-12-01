917ab9502014d01a7496e65c03108ad7e0f8925c
Enio Nicael <enionicael@gmail.com>
2020-12-01 19:08:55 -0300

    Solucao: desafio5 calcula total de pontos de um time

--- src/challenges.js
+++ src/challenges.js
@@ -34,8 +34,9 @@ function concatName(names) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let totalPoints = (wins * 3) + ties;
+  return totalPoints;
 }
 
 // Desafio 6
