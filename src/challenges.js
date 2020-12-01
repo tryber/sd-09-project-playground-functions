f53bdffd5d976b542832c6cd90af6806c2ff4004
devfelipesantiago <felipesantiagolds@gmail.com>
2020-12-01 18:58:03 -0300

    Exercicio 7

--- src/challenges.js
+++ src/challenges.js
@@ -44,9 +44,15 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if ((cat1 - mouse) > (cat2 - mouse)) {
+    return 'cat2'
+  } else if ((cat1 - mouse) < (cat2 - mouse)) {
+    return 'cat1'
+  }
+  return 'os gatos trombam e o rato foge';
 }
+
 
 // Desafio 8
 function fizzBuzz() {
