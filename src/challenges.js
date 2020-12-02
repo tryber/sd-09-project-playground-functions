9cecc2d3b6ed2960c97e4f678e7f539bfb9c293f
devfelipesantiago <felipesantiagolds@gmail.com>
2020-12-02 13:20:01 -0300

    Correçao exercicio 4

--- src/challenges.js
+++ src/challenges.js
@@ -11,13 +11,13 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(sentence) {
-  return sentence.split(' ');
+  return sentence.split(" ");
 }
 
 // Desafio 4
 function concatName(array) {
   let result = [array[array.length - 1]];
-  for (let index = 0; index < array.length - 1; index += 1) {
+  for (let index = 0; index < 1; index += 1) {
     result.push(array[index]);
   }
   return result.join();
@@ -25,7 +25,7 @@ function concatName(array) {
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return ((wins * 3) + (ties * 1));
+  return wins * 3 + ties * 1;
 }
 
 // Desafio 6
@@ -35,8 +35,8 @@ function highestCount(array) {
   for (let index = 0; index < array.length; index += 1) {
     if (array[index] > count) {
       count = array[index];
-    }
-    if (count === array[index]) {
+    } else {
+      count === array[index];
       repeat += 1;
     }
   }
@@ -45,12 +45,13 @@ function highestCount(array) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if ((cat1 - mouse) > (cat2 - mouse)) {
-    return 'cat2'
-  } else if ((cat1 - mouse) < (cat2 - mouse)) {
-    return 'cat1'
+  if (cat1 - mouse > cat2 - mouse) {
+    return "cat2";
   }
-  return 'os gatos trombam e o rato foge';
+  if (cat1 - mouse < cat2 - mouse) {
+    return "cat1";
+  }
+  return "os gatos trombam e o rato foge";
 }
 
 // Desafio 8
@@ -58,13 +59,15 @@ function fizzBuzz(array) {
   let result = [];
   for (let index = 0; index < array.length; index += 1) {
     if (array[index] % 3 === 0 && array[index] % 5 === 0) {
-      result.push('fizzBuzz')
-    } else if (array[index] % 5 === 0) {
-      result.push('Buzz')
-    } else if (array[index] % 3 === 0) {
-      result.push('fizz')
+      result.push("fizzBuzz");
+    }
+    if (array[index] % 5 === 0) {
+      result.push("Buzz");
+    }
+    if (array[index] % 3 === 0) {
+      result.push("fizz");
     } else {
-      result.push('bug!')
+      result.push("bug!");
     }
   }
   return result;
@@ -80,24 +83,23 @@ function decode() {
 
 // Desafio 10
 let java = {
-  tech: 'javascript',
-  nome: 'Felipe'
-}
+  tech: "javascript",
+  nome: "Felipe",
+};
 let react = {
-  tech: 'react',
-  nome: 'Felipe'
-}
+  tech: "react",
+  nome: "Felipe",
+};
 let python = {
-  tech: 'python',
-  nome: 'Felipe'
-}
+  tech: "python",
+  nome: "Felipe",
+};
 let nodejs = {
-  tech: 'nodejs',
-  nome: 'Felipe'
-}
-function techList(array){
+  tech: "nodejs",
+  nome: "Felipe",
+};
+function techList(array) {
   for (let key in array) {
-
   }
 }
 
