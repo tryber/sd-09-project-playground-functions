c9db928c79ff1f328182bc4096d7706b7f68132c
Matheus Schreiner <matheusrubisch@gmail.com>
2020-12-02 12:34:12 -0300

    Bonus 11

--- src/challenges.js
+++ src/challenges.js
@@ -185,7 +185,6 @@ function verificationNumber(arrayNumber) {
   }
   return true;
 }
-
 function transformeArrayString(arrayNumber) {
   let stringNumber = '';
   for (let number of arrayNumber) {
@@ -207,7 +206,7 @@ function generatePhoneNumber(arrayNumber) {
   }
   return 'array com tamanho incorreto';
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
+
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
