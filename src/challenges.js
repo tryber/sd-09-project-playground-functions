4eeef29a43055ef30d8251e2c042cc9fccb5291e
BBrolesi <brunohsbrolesi@gmail.com>
2020-12-01 22:12:05 -0300

    Check error in evaluator

--- src/challenges.js
+++ src/challenges.js
@@ -200,7 +200,6 @@ function generatePhoneNumber(numberArray) {
   return ('(' + numberArray.slice(0, 2) + ') ' + numberArray.slice(2, 7) + '-' + numberArray.slice(7, 11));
 }
 
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function checkSumRule(lineA, lineB, lineC) {
