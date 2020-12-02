18e437aa9b1fba11f6cf76bf7fa1ee7572f04344
Rafa Reis <rafaelrnascimento2006@gmail.com>
2020-12-02 14:14:14 -0300

    corrigindo issues apontadas pelo codeclimate

--- src/challenges.js
+++ src/challenges.js
@@ -106,10 +106,10 @@ function fizzBuzz(array) {
 
 // Desafio 9
 function encode(string) {
-  return (encodeDecode(string, 'a', '1', 'e', '2', 'i', '3,', 'o', '4', 'u', '5'));
+  return (encodeDecode(string, 'a', '1', 'e', '2', 'i', '3', 'o', '4', 'u', '5'));
 }
 function decode(string) {
-  return (encodeDecode(string, '1', 'a', '2', 'e', '3', 'i,', '4', 'o', '5', 'u'));
+  return (encodeDecode(string, '1', 'a', '2', 'e', '3', 'i', '4', 'o', '5', 'u'));
 }
 
 // Desafio 10
