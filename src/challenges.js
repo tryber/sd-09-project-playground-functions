024b3161d749deac91a875abe2035356418905ca
MarcosAlbuquerque <mpdsa20@gmail.com>
2020-12-02 10:38:12 -0300

    diminuição de erros do codeclimate

--- src/challenges.js
+++ src/challenges.js
@@ -34,48 +34,48 @@ function highestCount(param) {
   let ultimoNumero = numerosOrdenados[param.length - 1]
   let qtdNumerosRepetidos = 0;
 
-  //console.log( 'numero mais alto = ' + ultimoNumero )
+  // console.log( 'numero mais alto = ' + ultimoNumero )
 
   if (ultimoNumero === 0) {
     for (key in param) {
-      //console.log(param[key])
+      // console.log(param[key])
       if (param[key] === ultimoNumero) {
         ++qtdNumerosRepetidos
-}}
-
-    //console.log( 'quantidade de números 0 = ' + qtdNumerosRepetidos )
+      }
+    }
+    // console.log('quantidade de números 0 = ' + qtdNumerosRepetidos)
   } else {
-
-  for (let i = 0; i <= ultimoNumero; i++ ) {
-    if (param[i] === ultimoNumero) {
-      qtdNumerosRepetidos += 1
-}}}
-
+    for (let i = 0; i <= ultimoNumero; i += 1) {
+      if (param[i] === ultimoNumero) {
+        qtdNumerosRepetidos += 1
+      }
+    }
+  }
   return qtdNumerosRepetidos
 }
 
-//console.log( highestCount([9, 1, 2, 3, 9, 5, 7, 5, 4, 6, 9, 2, 9, 12, 9]) )
-//console.log( highestCount([9, 1, 2, 3, 9, 5, 7]) )
-//console.log( highestCount([0,0,0,0]) )
+// console.log(highestCount([9, 1, 2, 3, 9, 5, 7, 5, 4, 6, 9, 2, 9, 12, 9]))
+// console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
+// console.log(highestCount([0,0,0,0]))
 
-// Desafio 7
+// Desafio 7 - PASS
 function catAndMouse(mouse, cat1, cat2) {
   let distanciaGato1 = 0
   let distanciaGato2 = 0
 
-  if (mouse === cat1 && mouse === cat2) return "os gatos trombam e o rato foge"
-  if (mouse === cat1 && mouse != cat2) return "cat1"
-  if (mouse === cat2 && mouse != cat1) return "cat2"
+  if (mouse === cat1 && mouse === cat2) return 'os gatos trombam e o rato foge'
+  if (mouse === cat1 && mouse !== cat2) return 'cat1'
+  if (mouse === cat2 && mouse !== cat1) return 'cat2'
   if (mouse > cat1) {
-    for (let i = mouse; i > cat1; i = i - 1) {
+    for (let i = mouse; i > cat1; i -= 1) {
       distanciaGato1 += 1;
     }
   } else if (mouse < cat1) {
-    for (let i = mouse; i < cat1; i = i + 1) {
+    for (let i = mouse; i < cat1; i += 1) {
       distanciaGato1 += 1;
     }
   }
-  //console.log('Distancia do Gato 1: ' + distanciaGato1)
+  // console.log('Distancia do Gato 1: ' + distanciaGato1)
   if (mouse > cat2) {
     for (let i = mouse; i > cat2; i = i - 1) {
       distanciaGato2 += 1;
@@ -85,7 +85,7 @@ function catAndMouse(mouse, cat1, cat2) {
       distanciaGato2 += 1;
     }
   }
-  //console.log('Distancia do Gato 2: ' + distanciaGato2)
+  // console.log('Distancia do Gato 2: ' + distanciaGato2)
   if (distanciaGato1 > distanciaGato2) {
       return "cat2"
   } else if (distanciaGato2 > distanciaGato1) {
@@ -96,10 +96,10 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 
-//console.log( catAndMouse(4, 5, 1) )
-//console.log( catAndMouse(1, 0, 2) ) //precisar ser igual
-//console.log( catAndMouse(10, 4, 22) )
-//console.log( catAndMouse(0, 3, 2) )
+// console.log( catAndMouse(4, 5, 1) )
+// console.log( catAndMouse(1, 0, 2) ) //precisar ser igual
+// console.log( catAndMouse(10, 4, 22) )
+// console.log( catAndMouse(0, 3, 2) )
 
 // Desafio 8
 function fizzBuzz() {
