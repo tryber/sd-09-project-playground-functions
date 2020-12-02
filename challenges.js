81750100175f529d9957452c07844ccc3e08d40d
João Pedro Barros <joaopedrobarros5@gmail.com>
2020-12-02 02:31:03 -0300

    consertando requisitos 1,2,3 e 4

--- challenges.js
+++ challenges.js
@@ -1,19 +1,27 @@
 function compareTrue(v1,v2) {
   if (v1==true && v2==true) {
-    console.log("true")
+    console.log(true)
   } else {
-    console.log("false")
+    console.log(false)
   }
 };
 
 function calcArea(base, height) {
-  return((base*height)/2)
+  return (base * height)/2
 };
 
 function splitSentence(String) {
-  return (String.split(" "))
+  return String.split(" ")
 };
 
-function concatName(Ar[]) {
-  return (Ar.indexOf(-1) + ", " + Ar.indexOf(0))
+function concatName(Ar) {
+  return Ar.indexOf(-1) + ", " + Ar.indexOf(0)
 };
+
+function footballPoints(wins, ties) {
+  return "O time fez na temporada " + ((wins*3)+(ties)) + "pontos"
+};
+
+function highestCount(nums) {
+  return ()
+}
