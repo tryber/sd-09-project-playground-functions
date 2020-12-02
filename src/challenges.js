95829c98452f0e0f8ba618b5b32777cfd37491f4
Renan Costa <renancosta21@gmail.com>
2020-12-01 23:03:30 -0300

    requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -22,8 +22,8 @@ function concatName(stringsArray) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
