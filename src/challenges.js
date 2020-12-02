60c49e7c030e9f69222f844426a62197642ceda6
Micaela Alves <micaela.alves93@gmail.com>
2020-12-02 16:43:27 -0300

    Desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -77,21 +77,19 @@ function catAndMouse(mouse, cat1, cat2) {
 function fizzBuzz(arraynumbers) {
   let arrayFizzBuzz = [];
     for (let index = 0; index < arraynumbers.length; index += 1) {
-      if (arraynumbers[index] % 3 === 0) {
-        if (arraynumbers[index] % 5 === 0) {
-          arrayFizzBuzz.push('fizzBuzz');
-          continue;
-        }
-        arrayFizzBuzz.push('fizz');
+      if (arraynumbers[index] % 3 === 0 && arraynumbers[index] % 5 === 0) {
+        arrayFizzBuzz.push('fizzBuzz');
       } else if (arraynumbers[index] % 5 === 0) {
         arrayFizzBuzz.push('buzz');
+      } else if (arraynumbers[index] % 3 === 0) {
+        arrayFizzBuzz.push('fizz');
       } else {
         arrayFizzBuzz.push('bug!');
       }      
     }
     return arrayFizzBuzz;
   }
-
+//console.log(fizzBuzz([2, 15, 7, 9, 45]));
 // Desafio 9
 function encode(string) {
   let strEnc = '';
