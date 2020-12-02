df6e5f2a27dc5a2a7d04bfc007fd3e354949cb48
Layo Kaminski <layokaminski@gmail.com>
2020-12-02 13:43:57 -0300

    Requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -106,11 +106,25 @@ function encode(string) {
 return newString;
 }
 
-function decode() {
+function decode(string) {
   // seu código aqui
+  let newString = '';
+  let vowels = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  for (let key in string) {
+    if (string[key] === '1' || string[key] === '2' || string[key] === '3' || string[key] === '4'|| string[key] === '5') {
+      newString += vowels[string[key]];
+  } else {
+    newString += string[key];
+  }
 }
-
-console.log(decode('h3 th2r2!'));
+return newString;
+}
 
 // Desafio 10
 function techList(array, name) {
