5839815c9ec82c9a02bd70040a12b8524afaebd5
João Pedro Barros <joaopedrobarros5@gmail.com>
2020-12-02 01:50:32 -0300

    requisitos 1 e 2

---
+++ challenges.js
@@ -0,0 +1,11 @@
+function compareTrue(v1,v2) {
+  if (v1==true && v2==true) {
+    console.log("true")
+  } else {
+    console.log("false")
+  }
+};
+
+function calcArea(base, height) {
+  return((base*height)/2)
+};
