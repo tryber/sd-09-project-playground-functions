f912f9d0185d2fe5d46394dd3e15d7369cac1c82
pauloeliezerg <pauloeliezerg@gmail.com>
2020-12-02 18:40:39 -0300

    Added 5th challenge

--- src/challenges.js
+++ src/challenges.js
@@ -38,8 +38,9 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  points = (wins * 3) + ties;
+  return points;
 }
 
 // Desafio 6
