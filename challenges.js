d174ac29281eb0d9318be6d88378a18fe9a7962d
João Pedro Barros <joaopedrobarros5@gmail.com>
2020-12-02 15:06:25 -0300

    consertando requisitos do code climate

--- challenges.js
+++ challenges.js
@@ -1,45 +1,45 @@
-//Requisito 1
-function compareTrue(v1,v2) {
-  if (v1==true && v2==true) {
-    return true
-  } else {
-    return false
+// Requisito 1
+function compareTrue(v1, v2) {
+  if (v1===true && v2===true) {
+    return true;
+  } {
+    return false;
   }
-};
+}
 
-//Requisito 2
+// Requisito 2
 function calcArea(base, height) {
-  return (base * height)/2
-};
+  return (base * height) / 2;
+}
 
-//Requisito 3
+// Requisito 3
 function splitSentence(String) {
-  return String.split(" ")
-};
+  return String.split(' ');
+}
 
-//Requisito 4
+// Requisito 4
 function concatName(Ar) {
-  return Ar.indexOf(-1) + ", " + Ar.indexOf(0)
-};
+  return Ar.indexOf(-1) + ', ' + Ar.indexOf(0);
+}
 
-//Requisito 5
+// Requisito 5
 function footballPoints(wins, ties) {
-  return "O time fez na temporada " + ((wins*3)+(ties)) + "pontos"
-};
+  return 'O time fez na temporada ' + ((wins*3) + (ties)) + ' pontos';
+}
 
-//Requisito 6
+// Requisito 6
 function highestCount(nums) {
-  let numMax = 0
-  let numMaxTimes = 0
-  for (let index=0 ; index<nums.lenght() ; index += 1 ) {
-     if (numMax > nums.indexOf(index)) {
-       numMax = nums.indexOf(index)
-     };
-  };
-  for (index=0 ; index<nums.lenght() ; index += 1 ) {
-    if (numMax = nums.indexOf(index)) {
-      numMaxTimes += 1
-    };
-  };
-  return "O maior número é " + numMax + " e ele foi encontrado " + numMaxTimes + " vezes."
-};
+  let numMax = 0;
+  let numMaxTimes = 0;
+  for (let index = 0; index < nums.lenght(); index += 1) {
+    if (numMax > nums.indexOf(index)) {
+      numMax = nums.indexOf(index);
+     }
+  }
+  for (let index2 = 0; index2 < nums.lenght(); index2 += 1) {
+    if (numMax == nums.indexOf(index2)) {
+      numMaxTimes += 1;
+    }
+  }
+  return 'O maior número é ' + numMax + ' e ele foi encontrado ' + numMaxTimes + ' vezes.';
+}
