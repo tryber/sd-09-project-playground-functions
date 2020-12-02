f82042548b310fe0caaf5f655c4309bfaad893c3
Micaela Alves <micaela.alves93@gmail.com>
2020-12-01 21:14:34 -0300

    Ajustes

--- src/challenges.js
+++ src/challenges.js
@@ -3,7 +3,7 @@
 function compareTrue(value1, value2) {
   if (value1 === true && value2 === true) {
     return true;
-}
+  }
   return false;
 }
 
@@ -17,7 +17,7 @@ function calcArea(base, heigth) {
 // Desafio 3
 function splitSentence(string) {
   let result = '';
-  result = string.split(" ");
+  result = string.split('   ');
   return result;
 }
 
@@ -27,13 +27,13 @@ function concatName(arrayStrings) {
   let last = '';
   first = arrayStrings[0];
   last = arrayStrings[arrayStrings.length - 1];
-  return last + ", " + first;
+  return last , first;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   let result = 0
-  result = (wins *3) + (ties * 1);
+  result = (wins * 3) + (ties * 1);
   return result
 }
 
@@ -42,41 +42,41 @@ function highestCount(numbers) {
   let maxnumber = 0;
   let contnumber = 0;
   for (let index = 0; index < numbers.length; index += 1) {
-      if (maxnumber < numbers[index]) {
-          maxnumber = numbers[index];
-          contnumber = 1;
-      } else if (maxnumber === numbers[index]) {
-          contnumber += 1;
-      }
+    if (maxnumber < numbers[index]) {
+      maxnumber = numbers[index];
+      contnumber = 1;
+    } else if (maxnumber === numbers[index]) {
+      contnumber += 1;
+    }
   }
-return contnumber
+  return contnumber
 }
 
 // Desafio 7
 function catAndMouse(cat1, cat2, mouse) {
   if (cat1 === cat2) {
     return 'os gatos trombam e o rato foge';
-  }else{
+  } else {
     let distaCat1 = 0
     let distaCat2 = 0
-    if(mouse > cat1) {
-        distaCat1 = mouse - cat1;
+    if (mouse > cat1) {
+      distaCat1 = mouse - cat1;
     } else {
-        distaCat1 = cat1 - mouse;
+      distaCat1 = cat1 - mouse;
     }
-    if (mouse > cat2){
-        distaCat2 = mouse - cat2;
+    if (mouse > cat2) {
+      distaCat2 = mouse - cat2;
     } else {
-        distaCat2 = cat2 - mouse;
+      distaCat2 = cat2 - mouse;
     }
     if (distaCat1 < distaCat2) {
-        return 'cat1';
+      return 'cat1';
     } else {
-        return 'cat2';
+      return 'cat2';
     }
   }
 }
-console.log (catAndMouse(0, 3, 2));
+
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
