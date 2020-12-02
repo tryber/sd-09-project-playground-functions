93c902e00351e0554880d2987e3a7ff2d3459319
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2020-12-02 13:17:00 -0300

    Desafio 4 (sem hashtag pois estava mencionando o pull request dos coleguinhas

--- src/challenges.js
+++ src/challenges.js
@@ -27,8 +27,8 @@ function splitSentence(sentence) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return array[array.length - 1] + ', ' + array[0]
 }
 
 // Desafio 5
