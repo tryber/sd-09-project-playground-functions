52783980b273222d3244121b5b4d109de21d49e6
Renan Costa <renancosta21@gmail.com>
2020-12-01 21:50:48 -0300

    requisito 4

--- src/challenges.js
+++ src/challenges.js
@@ -18,10 +18,8 @@ function splitSentence(sentence) {
 
 // Desafio 4
 function concatName(stringsArray) {
-  stringsArray = ['teste1', 'teste2', 'teste3', 'teste4'];
-  let result = '';
-  result += `${stringsArray[stringsArray.length -1]}${', '}${stringsArray[0]}`;
-  return result;
+  stringsArray = [];
+  return `${stringsArray[stringsArray.length -1]}, ${stringsArray[0]}`;
 }
 
 // Desafio 5
