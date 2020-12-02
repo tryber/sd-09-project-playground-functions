a58e88a8a93dee910026918fa7ca182b4d7e9683
Micaela Alves <micaela.alves93@gmail.com>
2020-12-02 10:59:11 -0300

    Requisito 8

--- src/challenges.js
+++ src/challenges.js
@@ -77,16 +77,14 @@ function catAndMouse(mouse, cat1, cat2) {
 function fizzBuzz(arraynumbers) {
   let arrayFizzBuzz = [];
   for (let index = 0; index < arraynumbers.length; index += 1) {
-    if (arraynumbers[index] % 3 === 0) {
-      if (arraynumbers[index] % 5 === 0) {
-        arrayFizzBuzz.push('fizbuzz');
-      } else {
-        arrayFizzBuzz.push('fizz');
-      }
+    if (arraynumbers[index] % 3 === 0 && arraynumbers[index] % 5 === 0) {
+      arrayFizzBuzz.push('fizzBuzz');
+    } else if (arraynumbers[index] % 3 === 0) {
+      arrayFizzBuzz.push('fizz');
     } else if (arraynumbers[index] % 5 === 0) {
       arrayFizzBuzz.push('buzz');
     } else {
-      arrayFizzBuzz.push('bug!'); 
+      arrayFizzBuzz.push('bug!');
     }
   }
   return arrayFizzBuzz;
