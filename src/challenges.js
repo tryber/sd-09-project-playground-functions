02e29250ba08bbb2076f47b45cf7f99b2e18760e
Felipe Pimentel <lipe.pimentel89@gmail.com>
2020-12-02 12:11:19 -0300

    desafios 1-5, 7 e 8

--- src/challenges.js
+++ src/challenges.js
@@ -1,27 +1,35 @@
 // Project Playground Functions - by Felipe Pimentel
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(param1, param2) {
+  if (param1 === true && param2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = base * height / 2;
+  return area;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentence) {
+  arraySplit = sentence.split(" ");
+  return arraySplit;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(entry) {
+  let name = entry[entry.length - 1] + ', ' + entry[0];
+  return name;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = 3 * wins + 1 * ties;
+  return points;
 }
 
 // Desafio 6
@@ -30,13 +38,33 @@ function highestCount() {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
+      return 'cat1';
+  } else if (Math.abs(cat2 - mouse) < Math.abs(cat1 - mouse)) {
+      return 'cat2';
+  } else if (Math.abs(cat2 - mouse) === Math.abs(cat1 - mouse)) {
+      return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(entry) {
+  let divisibles = [];
+  
+  for (let index = 0; index < entry.length; index += 1) {
+      if (entry[index] % 3 === 0 && entry[index] % 5 === 0) {
+          divisibles.push('fizzBuzz');
+      } else if (entry[index] % 3 === 0) {
+          divisibles.push('fizz');
+      } else if (entry[index] % 5 === 0) {
+          divisibles.push('buzz');
+      } else {
+          divisibles.push('bug!')
+      }
+  }
+  
+  return divisibles;
 }
 
 // Desafio 9
