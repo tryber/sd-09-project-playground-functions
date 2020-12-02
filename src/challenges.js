34346cfd33e9c1675bccf5407ebca3bb643158ea
Carine Becker <caarine.m@gmail.com>
2020-12-01 23:23:49 -0300

    desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -5,36 +5,52 @@ function compareTrue(value1, value2) {
     return true
   }
   return false
-}
+};
 
 // Desafio 2
 function calcArea(base, height) {
   return (base * height) / 2
-}
+};
 
 // Desafio 3
 function splitSentence(string) {
   return string.split(' ')
-}
+};
 
 // Desafio 4
 function concatName(array) {
   return `${array[array.length - 1]}, ${array[0]}`
-}
+};
 
 // Desafio 5
 function footballPoints(wins, ties) {
   return (wins * 3) + ties
-}
+};
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+function highestCount(param) {
+  let major = [];
+  for (let index = 0; index < param.length; index += 1) {
+    if (param[index] >= major) {
+      major.push(param[index])
+    }
+  }
+  return major.length
+};
+
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanceCat1 = mouse - cat1
+  let distanceCat2 = mouse - cat2
+
+  if (distanceCat1 > distanceCat2) {
+    return 'cat1'
+  } else if (distanceCat2 > distanceCat1) {
+    return 'cat2'
+  } else if (distanceCat1 === distanceCat2) {
+    return 'Os gatos trombam e o rato foge'
+  }
 }
 
 // Desafio 8
