13cd66e2ea1adf65751eb712cafed0e1961b0d34
Bernardo Smith <bernardostech@gmail.com>
2020-12-02 10:04:18 -0500

    novos exercicios

--- src/challenges.js
+++ src/challenges.js
@@ -7,39 +7,63 @@ function compareTrue(valor1, valor2) {
 }
 // Desafio 2
 function calcArea(base, height) {
-  return (base * height) /2;
+  return (base * height)/2;
 }
 
 // Desafio 3
 function splitSentence(string) {
-  return string.split(" ");
+  return string.split(' ');
 }
 
 // Desafio 4
 function concatName(strings) {
-  return strings[-1], strings[0];
+  return strings.reverse[0].concat(' ', strings[0]);
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontos = (wins * 3) + (ties * 1);
+  return pontos;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let maior = 0;
+  let qtidade = 0;
+  for (let key in array) {
+    if (array[ḱey] > maior) {
+      maior = array[key];
+    } else if (maior === array[key]) {
+      qtidade += 1;
+    }
+  }return qtidade;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(dist1, dist2) {
+  if (dist1 > dist2) {
+    return 'cat1';
+  }else if (dist2 > dist1) {
+    return 'cat2';
+  }
+  return 'os gatos trombam e o gato foge';
 }
+
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
-
+function fizzBuzz(arrayNumbers) {
+  for (let key = 0; key < arrayNumbers.length; key += 1) {
+    if (arrayNumbers[key] % 3 === 0 && arrayNumbers[key] % 5 === 0) {
+       console.log('fizzBuzz');
+    }else if (arrayNumbers[key] % 3 === 0){
+      console.log('fizz');
+    }else if (arrayNumbers[key] % 5 === 0) {
+      console.log('buzz');
+    }
+      console.log("bug");
+    }
+ }
+    
 // Desafio 9
 function encode() {
   // seu código aqui
