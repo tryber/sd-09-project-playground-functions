10e164d56a1572b0d0d4af08773c81f4ab79d5aa
LeoMajevski <leonardo.majevski@gmail.com>
2020-12-02 15:08:26 -0300

    7. Caça ao rato v2

--- src/challenges.js
+++ src/challenges.js
@@ -53,15 +53,14 @@ function highestCount(numbers) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let distanceCat1 = cat1 - mouse;
-  let distanceCat2 = cat2 - mouse;
+  let distanceCat1 = Math.abs(cat1 - mouse);
+  let distanceCat2 = Math.abs(cat2 - mouse);
   if (distanceCat1 < distanceCat2) {
     return 'cat1';
   } else if (distanceCat1 === distanceCat2) {
     return 'os gatos trombam e o rato foge';
-  } else {
-    return 'cat2';
   }
+    return 'cat2';
 }
 
 // Desafio 8
