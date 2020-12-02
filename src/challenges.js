13105b8214b362098103f770d4123538387bc50d
Matheus Martino <matheus.martino97@gmail.com>
2020-12-02 01:01:38 -0300

    Fixing the 11th challenge

--- src/challenges.js
+++ src/challenges.js
@@ -211,7 +211,7 @@ function generatePhoneNumber(array) {
     isValid = false;
   }
 
-  if ((isValid = true)) {
+  if (isValid === true) {
     result = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
     return result;
   } else {
