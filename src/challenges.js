c3faed35a59d0548158bcf0afe188a9181e69f94
lcgoandete <lcgoandete@hotmail.com>
2020-12-02 07:30:42 -0300

    exercicio-8/3

--- src/challenges.js
+++ src/challenges.js
@@ -77,6 +77,11 @@ function fizzBuzz(array3) {
   let array = [];
 
   for (const multiplo of array3) {
+    verificarMultiplo(multiplo);
+  }
+  return array;
+
+  function verificarMultiplo(multiplo) {
     if ((multiplo % 3 === 0) && (multiplo % 5 !== 0)) {
       array.push('Fizz');
     } else if ((multiplo % 5 === 0) && (multiplo % 3 !== 0)) {
@@ -87,7 +92,6 @@ function fizzBuzz(array3) {
       array.push('bug!');
     }
   }
-  return array;
 }
 
 // Desafio 9
