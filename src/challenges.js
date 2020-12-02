ea99b17264c42ea979aedacaf453f370cd7b8619
Matheus Martino <matheus.martino97@gmail.com>
2020-12-01 23:48:21 -0300

    Solving the 11th challenge

--- src/challenges.js
+++ src/challenges.js
@@ -185,9 +185,40 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let reps
+  let isValid
+  let result
+
+  for (let i of array) {
+    reps = 0
+    
+    for (let j of array) {
+      if (i === j) {
+        reps++
+      }
+    }
+    
+    if (reps >= 3 || i < 0 || i > 9) {
+      result = 'não é possível gerar um número de telefone com esses valores'
+      isValid = false
+    }
+  }
+
+  if (array.length !== 11) {
+    result = 'Array com tamanho incorreto'
+    isValid = false
+  } 
+
+  if (isValid = true) {
+    result = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`
+    return result
+  } else {
+    return result
+  }
 }
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 1, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck() {
