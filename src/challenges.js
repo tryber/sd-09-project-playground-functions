94bfc1485a8a7278a84b07e033dad5855b6a949f
Maximilian Kaden <mkadenfilgo@gmail.com>
2020-12-02 02:23:26 -0300

    Desafio 8 - Refatoramento

--- src/challenges.js
+++ src/challenges.js
@@ -87,18 +87,23 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
+function checkBuzzFizzBug() {
+  if (numbers[index] % 5 === 0) {
+    arrayToReturn.push('buzz')
+  } else if (numbers[index] % 3 === 0) {
+    arrayToReturn.push('fizz')
+  } else {
+    arrayToReturn.push('bug!')
+  }
+}
 
 function fizzBuzz(numbers) {
   let arrayToReturn = []
   for (let index in numbers) {
     if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0) {
       arrayToReturn.push('fizzBuzz')
-    } else if (numbers[index] % 5 === 0) {
-      arrayToReturn.push('buzz')
-    } else if (numbers[index] % 3 === 0) {
-      arrayToReturn.push('fizz')
     } else {
-      arrayToReturn.push('bug!')
+      checkBuzzFizzBug()
     }
   }
   console.log(arrayToReturn)
