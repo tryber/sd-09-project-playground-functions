32dae211518158b1c47aff52ad686b5d2531703a
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-03 11:01:21 -0300

    corrige requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -64,12 +64,13 @@ function highestCount(array) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if (cat1 - mouse < cat2 - mouse) {
-    return 'cat1';
-  } else if (cat2 - mouse < cat1 - mouse) {
-    return 'cat2';
-  } 
-  return 'os gatos trombam e o rato foge';
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
+    return console.log('cat1');
+  } else if (Math.abs(cat2 - mouse) < Math.abs(cat1 - mouse)) {
+    return console.log('cat2');
+  } else if (Math.abs(cat1 - mouse) === Math.abs(cat2 - mouse)){
+    return console.log('os gatos trombam e o rato foge');
+  }
 }
 
 // Desafio 8
