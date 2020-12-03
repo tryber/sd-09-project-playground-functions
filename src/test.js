445df7238fb2112599c9f1330114ae910813c289
Daniela Muller <danielamuller20@gmail.com>
2020-12-03 19:22:17 -0300

    Desafio 4

--- src/test.js
+++
@@ -1,8 +0,0 @@
-// Desafio 4
-function concatName(array) {
-  let firstAndLast = [array[array.length - 1], array[0]];
-  firstAndLast = firstAndLast.join(', ');
-  return firstAndLast;
-}
-let arrayOfNames = ['Roberta', 'Julia', 'João', 'Maria', 'Marcos'];
-console.log(concatName(arrayOfNames));
