fe7546a98a8e2aad6e920163720dd4cbd3f67448
Matheus Martino <matheus.martino97@gmail.com>
2020-12-02 23:02:03 -0300

    Fixing the 11th challenge

--- src/challenges.js
+++ src/challenges.js
@@ -192,8 +192,9 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function repsValidation(array) {
+function generatePhoneNumber(array) {
   let isValid = true;
+  let result;
   let reps;
 
   for (let i of array) {
@@ -206,37 +207,18 @@ function repsValidation(array) {
     }
 
     if (reps >= 3 || i < 0 || i > 9) {
+      result = 'não é possível gerar um número de telefone com esses valores';
       isValid = false;
     }
   }
 
-  return isValid;
-}
-
-function checkIsValid(array) {
-  let isValid;
-
-  isValid = repsValidation(array);
-
   if (array.length !== 11) {
+    result = 'Array com tamanho incorreto.';
     isValid = false;
   }
 
-  return isValid;
-}
-
-function generatePhoneNumber(array) {
-  let result;
-  let isValid = true;
-
-  isValid = checkIsValid(array);
-
   if (isValid === true) {
     result = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
-  } else if (array.length !== 11) {
-    result = 'Array com tamanho incorreto.';
-  } else {
-    result = 'não é possível gerar um número de telefone com esses valores';
   }
   return result;
 }
--- teste.js
+++ teste.js
@@ -1,49 +1,32 @@
-function repsValidation(array) {
-  let isValid = true;
-  let reps;
-
-  for (let i of array) {
-    reps = 0;
-
-    for (let j of array) {
-      if (i === j) {
-        reps += 1;
+function generatePhoneNumber(array) {
+    let isValid = true;
+    let result;
+    let reps;
+  
+    for (let i of array) {
+      reps = 0;
+  
+      for (let j of array) {
+        if (i === j) {
+          reps += 1;
+        }
+      }
+  
+      if (reps >= 3 || i < 0 || i > 9) {
+        result = 'não é possível gerar um número de telefone com esses valores';
+        isValid = false;
       }
     }
-
-    if (reps >= 3 || i < 0 || i > 9) {
+  
+    if (array.length !== 11) {
+      result = 'Array com tamanho incorreto.';
       isValid = false;
     }
+  
+    if (isValid === true) {
+      result = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
+    }
+    return result;
   }
 
-  return isValid;
-}
-
-function checkIsValid(array) {
-  let isValid;
-
-  isValid = repsValidation(array);
-
-  if (array.length !== 11) {
-    isValid = false;
-  }
-
-  return isValid;
-}
-
-function generatePhoneNumber(array) {
-  let result;
-  let isValid = true;
-
-  isValid = checkIsValid(array);
-
-  if (isValid === true) {
-    result = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
-  } else if (array.length !== 11) {
-    result = 'Array com tamanho incorreto.';
-  } else {
-    result = 'não é possível gerar um número de telefone com esses valores';
-  }
-  return result;
-}
-console.log(generatePhoneNumber([1,2,3,4,5,6,9,9,9,0,1]))
+  console.log(generatePhoneNumber([1,2,3,4,5,6,7,8,9,0,1]))
