d34b343985defc9833938dc3a63e5a3696793487
Andrechiapetta <andreluizchiapetta@gmail.com>
2020-12-03 06:58:21 -0300

    Retirando um else do exercício 7

--- src/challenges.js
+++ src/challenges.js
@@ -49,9 +49,8 @@ function catAndMouse(mouse, cat1, cat2) {
     return 'cat1';
   } else if (distanceCat2 < distanceCat1) {
     return 'cat2';
-  } else {
-    return 'os gatos trombam e o rato foge';
   }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
