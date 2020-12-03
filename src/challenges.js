e95cea57ba3a62063767692625ca92562e2bfb7c
Matheus Mendes <matheus.unilog@gmail.com>
2020-12-03 10:43:43 -0300

    Exercises 4,5,6

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(boolean1, boolean2) {
-  if (boolean1 && boolean2 === true) {
+  if (boolean1 === true && boolean2 === true) {
     return true;
   }
   return false;
@@ -13,24 +13,48 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(string) {
-  const arrayString = string.split(' ');
+  const arrayString = string.split(" ");
   return arrayString;
 }
-console.log(splitSentence('Tree Of Savior'))
+console.log(splitSentence("Tree Of Savior"));
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(stringArray) {
+  let firstName = stringArray[0];
+  let lastName = stringArray[stringArray.length - 1];
+  return `${lastName}, ${firstName}`;
 }
+console.log(concatName(["Lucas", "Cassiano", "Ferraz", "Paolillo"]));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let winner = wins * 3;
+  let tied = ties * 1;
+  let points = winner + tied;
+  return points;
 }
+console.log(footballPoints(1, 1));
 
 // Desafio 6
-function highestCount() {
+function highestCount(numbers) {
   // seu código aqui
+  let contNumber = 0;
+  let contRepeat = 0;
+  let indexNumberRepeat = 0;
+  for (let index in numbers) {
+    let verifyNumber = numbers[index];
+    for (let index2 in numbers) {
+      if (verifyNumber === numbers[index2]) {
+        contRepeat++;
+      }
+    }
+    if (contNumber > contRepeat) {
+      contRepeat = contNumber;
+      indexNumberRepeat = index;
+    }
+  }
+  return numbers[indexNumberRepeat];
 }
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
 
 // Desafio 7
 function catAndMouse() {
