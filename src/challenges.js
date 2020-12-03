bc359e40c47171cc782edb7ecf688a3f003402eb
Victor Ricco <victor.ricco001@gmail.com>
2020-12-03 00:24:36 -0300

    add first challenge

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  
+function compareTrue(firstBoolean, secondBoolean) {
+  if(firstBoolean && secondBoolean){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 // Desafio 2
