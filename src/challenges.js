44f22cfde60493f74c9b10b1ab880c90a4058753
Victor Ricco <victor.ricco001@gmail.com>
2020-12-03 00:14:44 -0300

    firt commit test

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,12 @@
 // Desafio 1
 function compareTrue() {
-  // seu código aqui
+  
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
