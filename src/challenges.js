aba32212ea39cf30775c86dd24a2f0585a09c526
Luciano_Lodi <lrlodi@gmail.com>
2020-12-03 08:21:40 -0300

    Requisito 8

--- src/challenges.js
+++ src/challenges.js
@@ -73,8 +73,22 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers) {
+
+  let arrayFizzBuzz = [];
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 3 === 0){
+      arrayFizzBuzz.push('fizz');
+    } else if (numbers[i] % 5 === 0) {
+      arrayFizzBuzz.push('buzz');
+    } else if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0){
+      arrayFizzBuzz.push('fizzBuzz');
+    } else {
+      arrayFizzBuzz.push('bug!');
+    }
+  }
+  return arrayFizzBuzz;
 }
 
 // Desafio 9
