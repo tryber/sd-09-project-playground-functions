4c37edc25d32a1bf7697104c5d1b7fddbc7c69e8
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-03 10:26:58 -0300

    Adiciona funcao 7

--- src/challenges.js
+++ src/challenges.js
@@ -63,8 +63,14 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (cat1 - mouse < cat2 - mouse) {
+    return console.log ('Cat1');
+  } else if (cat2 - mouse < cat1 - mouse){
+    return console.log ('Cat2');
+  } else {
+    return console.log ('os gatos trombam e o rato foge')
+  }
 }
 
 // Desafio 8
