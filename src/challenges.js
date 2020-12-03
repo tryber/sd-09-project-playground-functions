4a9b334ad278823d8d1e9f23da3e7ebd13de05d6
João Pedro Barros <joaopedrobarros5@gmail.com>
2020-12-03 02:29:08 -0300

    requisitos 7 e 8

--- src/challenges.js
+++ src/challenges.js
@@ -36,7 +36,7 @@ function highestCount(nums) {
      }
   }
   for (let index2 = 0; index2 < nums.length; index2 += 1) {
-    if (numMax == nums[index2]) {
+    if (numMax === nums[index2]) {
       numMaxTimes += 1;
     }
   }
@@ -44,15 +44,33 @@ function highestCount(nums) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(mouse - cat1) != Math.abs(mouse - cat2)) {
+    if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)) {
+      return 'cat1';
+    }
+    return 'cat2';
+  }
+  return ('os gatos trombam e o rato foge');
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(Ar) {
+  let Array = [];
+  for (let index = 0; index < Ar.length; index += 1) {
+    if (Ar[index]%3 == 0 && Ar[index]%5 == 0) {
+      Array.push("fizzbuzz");
+    } else if (Ar[index]%3 == 0) {
+      Array.push("fizz");
+    } else if (Ar[index]%5 == 0) {
+      Array.push("buzz");
+    } else {
+      Array.push("bug");
+    }
+  }
+  return Array;
 }
-
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 // Desafio 9
 function encode() {
   // seu código aqui
