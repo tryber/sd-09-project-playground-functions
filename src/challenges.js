5d6862571e0094e9eaf913d197deefd39229ed7d
Pamela Anderson Sales <pamelasalesr@gmail.com>
2020-12-03 12:01:27 -0300

    Add chalenge 5

--- src/challenges.js
+++ src/challenges.js
@@ -38,14 +38,28 @@ function footballPoints(wins, ties) {
 console.log(footballPoints(3, 1));
 
 // Desafio 6
-function highestCount() {
+function highestCount(numbersArray) {
+  let highestNumber = 0;
+  let timesHighestNumberAppears = 0;
+  for (let index in numbersArray) {
+    if (numbersArray[index] > highestNumber) {
+      highestNumber = numbersArray[index];
+    }
+  }
 
+  for (let index in numbersArray) {
+    if (highestNumber === numbersArray[index]){
+      timesHighestNumberAppears += 1;
+    }
+  }
+  return timesHighestNumberAppears;
 }
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let cat1Distance = Math.abs(cat1 - mouse);
-  let cat2Distance = math.abs(cat2 - mouse);
+  let cat2Distance = Math.abs(cat2 - mouse);
   if (cat1Distance > cat2Distance) {
     return 'cat2';
   } else if (cat2Distance > cat1Distance) {
@@ -59,7 +73,7 @@ console.log(catAndMouse(1, 0, 2));
 function fizzBuzz(fizzBuzzArray) {
   let fizzBuzzResult = [];
   for (let index in fizzBuzzArray) {
-    if ((fizzBuzzArray[index] % 3 === 0) && (fizzBuzzArray[index] % 5 === 0)){
+    if ((fizzBuzzArray[index] % 3 === 0) && (fizzBuzzArray[index] % 5 === 0)) {
       fizzBuzzResult.push('fizzBuzz');
     } else if (fizzBuzzArray[index] % 3 === 0) {
       fizzBuzzResult.push('fizz');
@@ -74,11 +88,40 @@ function fizzBuzz(fizzBuzzArray) {
 console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
-function encode(stringToEnconde) {
-  
+function encode(arrayToEncode) {
+  let encodedArray = [];
+  for (let index = 0; index < arrayToEncode.length; index += 1) {
+    switch (arrayToEncode[index]){
+      case 'a':
+          encodedArray.push(arrayToEncode.replace(/a/gi, '1'));
+          break;
+
+      case 'e':
+          encodedArray.push(arrayToEncode.replace(/e/gi, '2'));
+          break;
+
+      case 'i':
+          encodedArray.push(arrayToEncode.replace(/i/gi, '3'));
+          break;
+      
+      case 'o':
+          encodedArray.push(arrayToEncode.replace(/o/gi, '4'));
+          break;
+
+      case 'u':
+          encodedArray.push(arrayToEncode.replace(/u/gi, '5'));
+          break;
+
+      default:
+          break;
+      }
+  }
+  return encodedArray.join('');
 }
+console.log(encode('hi there'));
+
 function decode() {
-  // seu código aqui
+ 
 }
 
 // Desafio 10
