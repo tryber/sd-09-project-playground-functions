8d4ee5f25c388076d606c0081580acb58092313e
Felipe Shcolnik <shcolnik@gmail.com.br>
2020-12-02 22:12:37 -0300

    incluindovariaveis dentro das funcoes func teste(variavel)

--- src/challenges.js
+++ src/challenges.js
@@ -15,14 +15,14 @@ function calcArea(base, height) {
 console.log(calcArea(3, 8));
 
 // Desafio 3
-function splitSentence(string) {
-  let arrayString = string.split(" ");
-  return arrayString;
+function splitSentence(string3) {
+  let arrayString3 = string3.split(" ");
+  return arrayString3;
 }
 
 // Desafio 4 OK
-function concatName(frase) {
-  let answer = (frase[frase.length-1] + ", " + frase[0]);
+function concatName(frase4) {
+  let answer = (frase4[frase4.length-1] + ", " + frase4[0]);
   return answer;
 }
 
@@ -34,24 +34,24 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestNumber(array) {
+function highestNumber(array6) {
   let index = 0;
-  let highest = array[0];
-    for (index = 0; index < array.length; index ++) {
-      if (highest < array[index]) {
-        highest = array[index];
+  let highest = array6[0];
+    for (index = 0; index < array6.length; index ++) {
+      if (highest < array6[index]) {
+        highest = array6[index];
       }
     }
   return highest;
 }
 
-let highest = highestNumber(array);
+let highest = highestNumber(array6);
 
-function highestCount(array) {
+function highestCount(array6) {
   let index = 0;
   let counter = 0;
-  for (index = 0; index < array.length; index ++) {
-    if (highest === array[index]) {
+  for (index = 0; index < array6.length; index ++) {
+    if (highest === array6[index]) {
       counter = counter + 1;
     }
   }
@@ -72,29 +72,42 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(array8) {
   let index = 0
-  for (index = 0; index < array.length; index ++) {
-    if (array[index] % 3 == 0 && array[index] % 5 == 0) {
-        array[index] = "fizzBuzz";
-    } else if (array[index] % 3 == 0) {
-      array[index] = "fizz";
-    } else if (array[index] % 5 == 0) {
-      array[index] = "buzz";
+  for (index = 0; index < array8.length; index ++) {
+    if (array8[index] % 3 == 0 && array8[index] % 5 == 0) {
+        array8[index] = "fizzBuzz";
+    } else if (array8[index] % 3 == 0) {
+      array8[index] = "fizz";
+    } else if (array8[index] % 5 == 0) {
+      array[index8] = "buzz";
     } else {
-    array[index] = "bug!";
+    array8[index] = "bug!";
     }
   }
-  return array;
+  return array8;
 }
 
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string9) {
+  let object = {"a":"1","e":"2","i":"3","o":"4","u":"5"};
+  for (let key in object) {
+    let substitute = key;
+    let re = new RegExp(substitute,"g");
+    string9 = string9.replace(re, object[key]);
+  }
+return string9;
 }
-function decode() {
-  // seu código aqui
+
+function decode(string9) {
+  let object = {"1":"a","2":"e","3":"i","4":"o","5":"u"};
+  for (let key in object) {
+    let substitute = key;
+    let re = new RegExp(substitute,"g");
+    string9 = string9.replace(re, object[key]);
+  }
+return string9;
 }
 
 // Desafio 10
@@ -104,7 +117,7 @@ function techList() {
 
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
+  let 
 }
 
 // Desafio 12
