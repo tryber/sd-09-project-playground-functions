2e035bb5d390d819ee8e2d5b9579467d4c82862a
Gabriel Castro <kastro.gabriel17@gmail.com>
2020-12-03 17:25:02 -0400

    Desafio 6: highestCount ajuste

--- src/challenges.js
+++ src/challenges.js
@@ -33,8 +33,8 @@ function footballPoints(wins, ties) {
 // Desafio 6
 function highestCount(arrayNumbers) {
   let count = 1;
-  arrayNumbers.sort();
   let index = arrayNumbers.length - 1;
+  arrayNumbers.sort();
   while (arrayNumbers[index] === arrayNumbers[index - 1]) {
     count += 1;
     index -= 1;
