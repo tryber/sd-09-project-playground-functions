d6bb9cfba9500c94eb9242c6551437a389e05b0d
Felipe Rocha <felipe.sr27@gmail.com>
2020-12-02 23:01:56 -0300

    Correcao de problemas apontados pelo codeclimate.

--- src/challenges.js
+++ src/challenges.js
@@ -1,14 +1,13 @@
 // Desafio 1
-function compareTrue(booleanOne ,booleanTwo) {
+function compareTrue(booleanOne, booleanTwo) {
   if (booleanOne === true && booleanTwo === true) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 // Desafio 2
-function calcArea(base ,height) {
+function calcArea(base, height) {
   let area = ((base * height) / 2);
   return area;
 }
@@ -21,12 +20,12 @@ function splitSentence(str) {
 
 // Desafio 4
 function concatName(array) {
-  let str = array[array.length - 1] + ', ' + array[0];
+  let str = `${array[array.length - 1]}, ${array[0]}`;
   return str;
 }
 
 // Desafio 5
-function footballPoints(wins ,ties) {
+function footballPoints(wins, ties) {
   let points = 0;
   points = (wins * 3) + ties;
   return points;
@@ -36,39 +35,37 @@ function footballPoints(wins ,ties) {
 function highestCount(array) {
   let higherNum = 0;
   let count = 0;
-  for (index in array){
-    if (array[index] >= higherNum){
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] >= higherNum) {
       count = 0;
       higherNum = array[index];
-      for (indexTwo in array){
-        if (higherNum === array[indexTwo]){
+      for (let indexTwo = 0; indexTwo < array.length; indexTwo += 1) {
+        if (higherNum === array[indexTwo]) {
           count += 1;
         }
       }
-    } 
+    }
   }
   return count;
 }
 
 // Desafio 7
-function catAndMouse(mouse ,cat1 ,cat2) {
+function catAndMouse(mouse, cat1, cat2) {
   let result = '';
   if ((Math.abs(cat1 - mouse)) === Math.abs((cat2 - mouse))) {
     result = 'os gatos trombam e o rato foge';
-    return result;
-  } else if ((Math.abs(cat1 - mouse)) < Math.abs((cat2 - mouse))){
+  } else if ((Math.abs(cat1 - mouse)) < Math.abs((cat2 - mouse))) {
     result = 'cat1';
-    return result;
-  } else if ((Math.abs(cat1 - mouse)) > Math.abs((cat2 - mouse))){
+  } else if ((Math.abs(cat1 - mouse)) > Math.abs((cat2 - mouse))) {
     result = 'cat2';
-    return result;
   }
+  return result;
 }
 
 // Desafio 8
 function fizzBuzz(array) {
   let result = [];
-  for (index in array){
+  for (let index = 0; index < array.length; index += 1) {
     if ((array[index] % 3 === 0) && (array[index] % 5 === 0)) {
       result.push('fizzBuzz');
     } else if (array[index] % 5 === 0) {
@@ -85,7 +82,7 @@ function fizzBuzz(array) {
 // Desafio 9
 function encode(str) {
   let array = str.split('');
-  for (index in array){
+  for (let index = 0; index < array.length; index += 1) {
     if (array[index] === 'a') {
       array[index] = 1;
     } else if (array[index] === 'e') {
@@ -104,7 +101,7 @@ function encode(str) {
 
 function decode(str) {
   let array = str.split('');
-  for (index in array){
+  for (let index = 0; index < array.length; index += 1) {
     if (array[index] === '1') {
       array[index] = 'a';
     } else if (array[index] === '2') {
@@ -122,21 +119,20 @@ function decode(str) {
 }
 
 // Desafio 10
-function techList(array ,name) {
+function techList(array, name) {
   let arrayTech = [];
-  if (array.length != 0){
+  if (array.length !== 0) {
     array = array.sort();
-    for (index in array){
+    for (let index = 0; index < array.length; index += 1) {
       let object = {
         tech: array[index],
-        name: name,
+        name,
       };
       arrayTech.push(object);
     }
     return arrayTech;
-  } else {
-    return 'Vazio!';
   }
+  return 'Vazio!';
 }
 
 // Desafio 11
@@ -144,48 +140,43 @@ function generatePhoneNumber(array) {
   let telephone = '';
   let error = 'não é possível gerar um número de telefone com esses valores';
   let errorArray = 'Array com tamanho incorreto.';
-  if (array.length === 11){
-    for (index in array) {
+  if (array.length === 11) {
+    for (let index = 0; index < array.length; index += 1) {
       let cont = 0;
-      for (index2 in array){
-        if (array[index] === array [index2]){
+      for (let index2 = 0; index2 < array.length; index2 += 1) {
+        if (array[index] === array[index2]) {
           cont += 1;
           if (cont >= 3){
             return error;
-            break;
           }
         }
       }
-      if ((array[index] < 0) || (array[index] > 9)){
+      if ((array[index] < 0) || (array[index] > 9)) {
         return error;
-        break;
-      } else if (index === '0') {
-        telephone += '(' + array[index];
-      } else if (index === '1') {
-        telephone += array[index] + ')' + ' ';
-      } else if (index === '6'){
-        telephone += array[index] + '-';
+      } else if (index === 0) {
+        telephone += `(${array[index]}`;
+      } else if (index === 1) {
+        telephone += `${array[index]}) `;
+      } else if (index === 6) {
+        telephone += `${array[index]}-`;
       } else {
         telephone += array[index];
       }
     }
     return telephone;
-  } else {
-    return errorArray;
   }
+  return errorArray;
 }
 
 // Desafio 12
-function triangleCheck(lineA ,lineB , lineC) {
-  if ((lineA > (lineB + lineC) || lineB > (lineA + lineC) || lineC > (lineA + lineC)) && (lineA > (Math.abs(lineB - lineC)) || lineB > (Math.abs(lineA - lineC)) || lineC > (Math.abs(lineA - lineC)))){
-    return false;
-  } else {
-    return true;
+function triangleCheck(lineA, lineB, lineC) {
+  if ((lineA > (lineB + lineC) || lineB > (lineA + lineC) || lineC > (lineA + lineC))) {
+    if ((lineA > (Math.abs(lineB - lineC)) || lineB > (Math.abs(lineA - lineC)) || lineC > (Math.abs(lineA - lineC)))){
+      return false;
+    }
   }
+  return true;
 }
-
-console.log(triangleCheck(10, 14, 8));
-console.log(triangleCheck(16, 9, 2));
 
 // Desafio 13
 function hydrate(str) {
@@ -193,13 +184,13 @@ function hydrate(str) {
   let numberOfDrinks = '';
   let numberOfWater = 0;
   numberOfDrinks = str.match(digit);
-  for (index in numberOfDrinks){
-    numberOfWater += parseInt(numberOfDrinks[index]);
+  for (let index = 0; index < numberOfDrinks.length; index += 1) {
+    numberOfWater += parseInt(numberOfDrinks[index], 10);
   }
-  if ( numberOfWater == 1){
-    str =  numberOfWater + ' copo de água';
+  if(numberOfWater === 1) {
+    str = `${numberOfWater} copo de água`;
   } else {
-    str =  numberOfWater + ' copos de água';
+    str = `${numberOfWater} copos de água`;
   }
   return str;
 }
