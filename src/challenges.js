b0bfb637d585bf2f49b7ab6a9363c55b686c29b6
Henrique-Moura <henrique_hgm1996@hotmail.com>
2020-12-04 12:31:38 -0300

    Desafio 7 - Caça ao rato

--- src/challenges.js
+++ src/challenges.js
@@ -62,9 +62,17 @@ console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  
+  let gato1 = cat1 - mouse;
+  let gato2 = cat2 - mouse;
+  if(gato1 > gato2){
+    return 'cat2';
+  } else if( gato2 > gato1){
+    return 'cat1';
+  } else {
+    return 'os gatos trombam e o rato foge';
+  }   
 }
-
+console.log(catAndMouse(1, 3, 2))
 // Desafio 8
 function fizzBuzz(arrayFizzBuzz) {
   let array1 = []
