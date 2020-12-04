df53a4003c8b0c5ef91fca30d67febce7547295b
FredericoCanoeira <fcanoeira@gmail.com>
2020-12-04 01:41:24 +0100

    tentativa 2 Desaio 7

--- src/challenges.js
+++ src/challenges.js
@@ -57,9 +57,9 @@ function catAndMouse(mouse, cat1, cat2) {
   let distanceCat1 = Math.abs(mouse - cat1);
   let distanceCat2 = Math.abs(mouse - cat2);
   switch (true) {
-    case distanceCat2 < distanceCat1:
-      return 'cat2';
     case distanceCat1 < distanceCat2:
+      return 'cat1';
+    case distanceCat2 < distanceCat1:
       return 'cat2';
     case distanceCat1 === distanceCat2:
       return 'os gatos trombam e o rato foge';
