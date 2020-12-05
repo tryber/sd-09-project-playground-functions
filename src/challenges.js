e201c73ccccabac8d8e9d1179561e5e644acd4da
Luiz Vicentin <luizpavanello@gmail.com>
2020-12-05 11:15:22 -0300

    Corrections

--- src/challenges.js
+++ src/challenges.js
@@ -69,66 +69,54 @@ function fizzBuzz(numbers) {
     }
   }
   return arrayFizzBuzz;
-};
+}
 
 // Desafio 9
-function encode(phrase) {
-  let encoded = '';
-  for (let index = 0; index < phrase.length; index += 1) {
-    switch (phrase[index]) {
-      case 'a':
-        encoded = encoded.concat('1');
-        break;
-      case 'e':
-        encoded = encoded.concat('2');
-        break;
-      case 'i':
-        encoded = encoded.concat('3');
-        break;
-      case 'o':
-        encoded = encoded.concat('4');
-        break;
-      case 'u':
-        encoded = encoded.concat('5');
-        break;
-      default:
-        encoded = encoded.concat(phrase[index]);
-        break;
-    }
-  }
-  return encoded;
-};
+function encode(string) {
+  let codedString = '';
 
-function decode(phrase) {
-  let decoded = '';
-  for (let index = 0; index < phrase.length; index += 1) {
-    switch (phrase[index]) {
-      case '1':
-        decoded = decoded.concat('a');
-        break;
-      case '2':
-        decoded = decoded.concat('e');
-        break;
-      case '3':
-        decoded = decoded.concat('i');
-        break;
-      case '4':
-        decoded = decoded.concat('o');
-        break;
-      case '5':
-        decoded = decoded.concat('u');
-        break;
-      default:
-        decoded = decoded.concat(phrase[index]);
-        break;
+  for (let i = 0; i < string.length; i += 1) {
+    if (string[i] === 'a') {
+      codedString += '1';
+    } else if (string[i] === 'e') {
+      codedString += '2';
+    } else if (string[i] === 'i') {
+      codedString += '3';
+    } else if (string[i] === 'o') {
+      codedString += '4';
+    } else if (string[i] === 'u') {
+      codedString += '5';
+    } else {
+      codedString += string[i];
     }
   }
-  return decoded;
+  return codedString;
+}
+
+function decode(codedString) {
+  let decodedString = '';
+
+  for (let i = 0; i < codedString.length; i += 1) {
+    if (codedString[i] === '1') {
+      decodedString += 'a';
+    } else if (codedString[i] === '2') {
+      decodedString += 'e';
+    } else if (codedString[i] === '3') {
+      decodedString += 'i';
+    } else if (codedString[i] === '4') {
+      decodedString += 'o';
+    } else if (codedString[i] === '5') {
+      decodedString += 'u';
+    } else {
+      decodedString += codedString[i];
+    }
+  }
+  return decodedString;
 }
 
 // Desafio 10
 function techList(techs, name) {
-  if (techs == '') {
+  if (techs === '') {
     return 'Vazio!';
   }
   let techListName = [];
