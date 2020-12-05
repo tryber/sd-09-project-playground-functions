14c8c6cfd7cf034794aaffb2b34e49461f762307
LeoMajevski <leonardo.majevski@gmail.com>
2020-12-05 14:19:06 -0300

    9. Codifique e Decodifique v1

--- src/challenges.js
+++ src/challenges.js
@@ -82,10 +82,42 @@ function fizzBuzz(arrayOfNumbers) {
 
 // Desafio 9
 function encode(string) {
-  
+  let encodedString = '';
+  for (let index in string) {
+    if (string[index] === 'a') {
+      encodedString += '1';
+    } else if (string[index] === 'e') {
+      encodedString += '2';
+    } else if (string[index] === 'i') {
+      encodedString += '3';
+    } else if (string[index] === 'o') {
+      encodedString += '4';
+    } else if (string[index] === 'u') {
+      encodedString += '5';
+    } else {
+      encodedString += string[index];
+    }
+  }
+  return encodedString;
 }
-function decode() {
-  
+function decode(string) {
+  let decodedString = '';
+  for (let index in string) {
+    if (string[index] === '1') {
+      decodedString += 'a';
+    } else if (string[index] === '2') {
+      decodedString += 'e';
+    } else if (string[index] === '3') {
+      decodedString += 'i';
+    } else if (string[index] === '4') {
+      decodedString += 'o';
+    } else if (string[index] === '5') {
+      decodedString += 'u';
+    } else {
+      decodedString += string[index];
+    }
+  }
+  return decodedString;
 }
 
 // Desafio 10
