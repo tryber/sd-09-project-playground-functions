6337c3f28cbdfe5abe62128ea04a0f66a85126fe
Carine Becker <caarine.m@gmail.com>
2020-12-05 16:24:23 -0300

    desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -51,7 +51,7 @@ function catAndMouse(mouse, cat1, cat2) {
   } else if (distanceCatTwo < distanceCatOne) {
     return 'cat2';
   }
-  return 'Os gatos trombam e o rato foge';
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
