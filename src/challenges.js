7be979bcb30c12cb279e2d8b151a685b553983e4
Ricardo Sbeghen <ricardo.sbeghen@gmail.com>
2020-12-05 17:50:05 -0300

    Continuando os exercicios

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,9 @@
 // Desafio 1
 function compareTrue(valor1, valor2){
-  if ((valor1 === true) && (valor2 === false) {
+  if ((valor1 === true) && (valor2 === true)) {
     return true;
-  } else {
-    return false;
   }
+    return false;
 }
 
 // Desafio 2
@@ -21,18 +20,21 @@ function splitSentence() {
 // Desafio 4
 function concatName(arrayString) {
   return arrayString[arrayString.lenght-1] + ", " + arrayString[0];
-  
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let vict = wins *3;
+  let draw = ties *1;
+  let points = vict + draw
+  
+  return points;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+function highestCount(arrayNumeros) {
+  arrayNumeros.sort((a, b) => b-a);
+
 
 // Desafio 7
 function catAndMouse() {
@@ -40,17 +42,57 @@ function catAndMouse() {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+    let arrayResult = [];
+
+    function fizzBuzz(arrayNumeros) {
+      for (let i = 0 ; i < arrayNumeros.length ; i +=1) {
+
+        if ((arrayNumeros[i] % 3 === 0) && (arrayNumeros[i]  % 5 ===0)) {
+            arrayResult.push ("fizzBuzz");
+        
+          } else if (arrayNumeros[i] % 3 === 0) {
+            arrayResult.push ("fizz");
+          
+          } else if (arrayNumeros[i] % 5 ===0) {
+            arrayResult.push ("buzz");
+          
+          } else {
+            arrayResult.push ('bug!')
+          }
+
+      }
+      console.log(arrayResult);
+    }
+    /// 
+
+
 
 // Desafio 9
-function encode() {
+
+function encode(fr4s3) {
+
+    fr4s3 = fr4s3.replaceAll ('a', '1');
+    fr4s3 = fr4s3.replaceAll ('e', '2');
+    fr4s3 = fr4s3.replaceAll ('i', '3');
+    fr4s3 = fr4s3.replaceAll ('o', '4');
+    fr4s3 = fr4s3.replaceAll ('u', '5');
+    
+    return fr4s3;
+    }
   // seu código aqui
+
+function decode(stringN) {
+
+  stringN = stringN.replaceAll ('1', 'a');
+  stringN = stringN.replaceAll ('2', 'e');
+  stringN = stringN.replaceAll ('3', 'i');
+  stringN = stringN.replaceAll ('4', 'o');
+  stringN = stringN.replaceAll ('5', 'u');
+    
+  return stringN;
 }
-function decode() {
-  // seu código aqui
-}
+
+
 
 // Desafio 10
 function techList() {
