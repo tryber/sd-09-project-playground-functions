fbe19ba2744fb1dbab9f1a6066c4f519831d198a
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2020-12-05 05:50:25 -0300

    Desafio 3 feito.

--- src/challenges.js
+++ src/challenges.js
@@ -13,8 +13,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(word) {
+  result = word.split(" ");
+  return result;
 }
 
 // Desafio 4
