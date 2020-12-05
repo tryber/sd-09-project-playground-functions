ccbec66c13d049946e70e3ab38f68e4d6af26319
Luiz Vicentin <luizpavanello@gmail.com>
2020-12-05 10:44:01 -0300

    Exercises 6 & 7 + Corrections

--- src/challenges.js
+++ src/challenges.js
@@ -4,7 +4,7 @@
 function compareTrue(value1, value2) {
   if (value1 === true && value2 === true) {
     return true;
-  } 
+} 
   return false;
 }
 
@@ -15,16 +15,14 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(string) {
-  let phrase = ' ';
-  phrase = string.split(' ');
-  return phrase;
+  return string.split(' ');
 }
 
 // Desafio 4
 function concatName(arrayString) {
   let firstItem = arrayString[0];
   let lastItem = arrayString[arrayString.length - 1];
-  return  `${lastItem}, ${firstItem}`;
+  return `${lastItem}, ${firstItem}`;
 }
 
 // Desafio 5
@@ -34,31 +32,28 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount(array) {
-  let greatest = array[0]
-  for (let element of array) {
-    if (element > greatest) {
-      greatest = element
+function highestCount(arrayNumbers) {
+    let arrayOrdenad = arrayNumbers.sort(compareNumbers);
+    let numberMax = arrayOrdenad.length - 1;
+    let cont = 0;
+    for (let index = 0; index < arrayOrdenad.length; index += 1) {
+      if (arrayOrdenad[index] === arrayOrdenad[numberMax]) {
+        cont += 1;
+      }
     }
-  }
-  return greatest
-}
-
-function highestCount(array) {
-  // seu código aqui
-  const greatestNumber = greatestValue(array)
-  let greatestCounter = 0
-  for (let element of array) {
-    if (element === greatestNumber) {
-      greatestCounter += 1
-    }
-  }
-  return greatestCounter
+    return cont;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distCat1 = Math.abs(cat1 - mouse);
+  let distCat2 = Math.abs(cat2 - mouse);
+  if (distCat1 === distCat2) {
+    return 'os gatos trombam e o rato foge';
+  } else if (distCat1 < distCat2) {
+    return 'cat1';
+  }
+  return 'cat2';
 }
 
 // Desafio 8
