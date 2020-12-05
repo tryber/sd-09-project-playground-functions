99a6c9282c9072c24abfe27307b6aedd6f7e2c48
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-05 11:18:28 -0300

    solving error chalenge bonus 11

--- src/challenges.js
+++ src/challenges.js
@@ -146,45 +146,86 @@ console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'] , 'Lucas'));
 function generatePhoneNumber(numbers) {
   let rN = []
   if(numbers.length < [11] || numbers.length > [11]) {
-    return 'Array com tamanho incorreto.';
+      return 'Array com tamanho incorreto.';
+    }
+  for(let index = 0; index < numbers.length; index += 1){
+    if(numbers[index] < 0 || numbers[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
   }
-for(let index = 0; index < numbers.length; index += 1){
-  if(numbers[index] < 0 || numbers[index] > 9) {
-    return 'não é possível gerar um número de telefone com esses valores';
+  for(let index = 0; index < numbers.length; index += 1) {
+  rN.push(numbers[index])  
+    }
+concat = '(' + rN[0] + rN[1] + ')' + ' ' + rN[2] + rN[3] + rN[4] + rN[5] + rN[6] + '-' + rN[7] + rN[8] + rN[9] + rN[10] + '.';
+  return(concat);
 }
-}
-for(let index = 0; index < numbers.length; index += 1) {
-  rN[index] = Math.floor( Math.random() * 9);
-  }
-  concat = '(' + rN[0] + rN[1] + ')' + ' ' + rN[2] + rN[3] + rN[4] + rN[5] + rN[6] + '-' + rN[7] + rN[8] + rN[9] + rN[10] + '.';
-    return(concat);
-  }
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9]));
 
+
+
+
+// Um triângulo é composto de três linhas: lineA, lineB e lineC. Crie uma função chamada triangleCheck que deverá receber as três linhas como parâmetro e retornar se é possível formar um triângulo com os valores apresentados de cada linha
+
+// Para tanto, tenha em mente algumas considerações:
+
+// Para que seja possível formar um triângulo, é necessário que a medida de qualquer um dos lados seja menor que a soma das medidas dos outros dois e maior que o valor absoluto da diferença entre essas medidas.
+
+// Um triângulo é composto de três linhas: lineA, lineB e lineC. Crie uma função chamada triangleCheck que deverá receber as três linhas como parâmetro e retornar se é possível formar um triângulo com os valores apresentados de cada linha
+// let lineA = 1;
+// let lineB = 2;
+// let lineC = 3;
+
+// if(lineA <= (lineB + lineC) || lineB <= (lineC + lineA) || lineC <= (lineB + lineA)) {
+//   console.log(true);
+// } else { 
+//   console.log(false)
+}
+
+
+// Para obter o valor absoluto de um número em JavaScript, pesquise pela função Math.abs.
+
+// O retorno da sua função deverá ser um booleano.
+
+// Exemplo: o retorno de triangleCheck(10, 14, 8) deverá ser true.
+
+
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
-
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+// function triangleCheck(lineA, lineB, lincC) {
+  
+// }
+// console.log(triangleCheck());
 
 
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  generatePhoneNumber,
-  techList,
-  highestCount,
-  hydrate,
-  splitSentence,
-  triangleCheck,
-}
+
+
+
+
+
+
+
+
+
+
+
+// // Desafio 13
+// function hydrate() {
+//   // seu código aqui
+// }
+
+
+// module.exports = {
+//   calcArea,
+//   catAndMouse,
+//   compareTrue,
+//   concatName,
+//   decode,
+//   encode,
+//   fizzBuzz,
+//   footballPoints,
+//   generatePhoneNumber,
+//   techList,
+//   highestCount,
+//   hydrate,
+//   splitSentence,
+//   triangleCheck,
+// }
