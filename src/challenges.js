7e1e6edfa38f9b12b90cd77fb472ee6be5ee3817
Leticia Lima <letylima96@gmail.com>
2020-12-05 16:51:18 -0300

    soluciona desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -91,7 +91,22 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {}
+function fizzBuzz(arrayNumbers) {
+  let array = [];
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    if (arrayNumbers[index] % 3 === 0 && arrayNumbers[index] % 5 !== 0) {
+      array.push('fizz');
+    } else if (arrayNumbers[index] % 5 === 0 && arrayNumbers[index] % 3 !== 0) {
+      array.push('buzz');
+    } else if (arrayNumbers[index] % 15 === 0) {
+      array.push('fizzBuzz');
+    } else {
+      array.push('bug!');
+    }
+  }
+  return array;
+}
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
 function encode() {
