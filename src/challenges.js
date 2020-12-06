965af998c807b6ef7db2910ea50f59340358f9ab
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2020-12-06 18:26:19 -0300

    Requisito 9, #VQV!

--- src/challenges.js
+++ src/challenges.js
@@ -83,12 +83,63 @@ function fizzBuzz(param) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+const text = 'hi there!';
+function encode(string) {
+  let result = []
+  for (index = 0; index < string.length; index+=1) {
+    switch (string[index]) {
+      case 'a':
+        result += 1;
+        break;
+      case 'e':
+        result += 2;
+        break;
+      case 'i':
+        result += 3;
+        break;
+      case 'o':
+        result += 4;
+        break;
+      case 'u':
+        result += 5;
+        break;
+      default:
+        result += string[index]
+        break;
+    }
+  }
+  return result;
 }
-function decode() {
-  // seu código aqui
+encode(text);
+
+const texto = 'h3 th2r2!';
+function decode(string) {
+  let result = []
+  for (index = 0; index < string.length; index+=1) {
+    switch (string[index]) {
+      case '1':
+        result += 'a';
+        break;
+      case '2':
+        result += 'e';
+        break;
+      case '3':
+        result += 'i';
+        break;
+      case '4':
+        result += 'o';
+        break;
+      case '5':
+        result += 'u';
+        break;
+      default:
+        result += string[index]
+        break;
+    }
+  }
+  return result;
 }
+decode(texto);
 
 // Desafio 10
 function techList() {
