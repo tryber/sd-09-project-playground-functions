23bb063cbe8623815bff54c8f662189a77e0af45
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-05 22:23:38 -0300

    corrige bug do exercicio 11

--- src/challenges.js
+++ src/challenges.js
@@ -161,7 +161,7 @@ function generatePhoneNumber(array) {
       return 'não é possível gerar um número de telefone com esses valores'
       break;
     }
-    for (let index2 = 0; index < array.length; index2 += 1){
+    for (let index2 = 0; index2 < array.length; index2 += 1){
       if (array[index] === array[index2]) {
         contador += 1;
       }
