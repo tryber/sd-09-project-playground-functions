1c878507cdb1d441d0663a9dd22e14498685b190
Emanoel Leão <emanuelleaomarinho@gmail.com>
2020-12-06 16:27:31 -0300

    Desafio 06 concluído!

--- src/challenges.js
+++ src/challenges.js
@@ -7,27 +7,22 @@ function compareTrue(comp1, comp2) {
   }
   return false;
 }
-compareTrue(1,1);
 
 // Desafio 2
 function calcArea(base, height) {
   return ((base * height) / 2);
 }
-// calcArea(3,5);
 
 // Desafio 3
 function splitSentence(string) {
   let stringSplited = string.split(' ');
   return stringSplited;
 }
-// splitSentence("go trybe");
 
 // Desafio 4
 function concatName(arrayString) {
   return `${arrayString[arrayString.length - 1]}, ${arrayString[0]}`;
 }
-// let array =  ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
-// concatName(array);
 
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -35,11 +30,22 @@ function footballPoints(wins, ties) {
   let tiesPoints = 1;
   return (wins * winsPoints) + (ties * tiesPoints);
 }
-// footballPoints(1,1);
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arrayNumber) {
+  let bigN = arrayNumber[0];
+  let contNumber = 0;
+  for (let index = 0; index < arrayNumber.length; index++) {
+    if (bigN < arrayNumber[index]) {
+      bigN = arrayNumber[index];
+    }
+  }
+  for (let index = 0; index < arrayNumber.length; index++) {
+    if (bigN === arrayNumber[index]) {
+      contNumber += 1;
+    }
+  }
+  return contNumber;
 }
 
 // Desafio 7
