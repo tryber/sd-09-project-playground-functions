827a5351f68e0403dedde732b8404e63b51a1b48
Renato Benage <renatobenage@gmail.com>
2020-12-07 00:03:31 -0300

    Requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -47,6 +47,16 @@ function highestCount(param1) {
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  let distanceCat1 = Math.abs(cat1 - mouse)
+  let distanceCat2 = Math.abs(cat2 - mouse)
+
+  if (distanceCat1 < distanceCat2) {
+    return "cat1"
+  } else if (distanceCat2 < distanceCat1) {
+    return "cat2"
+  } else {
+    return "os gatos trombam e o rato foge"
+  }
 }
 
 // Desafio 8
