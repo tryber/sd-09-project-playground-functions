ffa8835bb3eb992db2ccbceba8596beb04e960a0
Emanoel Leão <emanuelleaomarinho@gmail.com>
2020-12-07 19:19:25 -0300

    Desafio 11: Reparando erros reportados!

--- src/challenges.js
+++ src/challenges.js
@@ -191,7 +191,7 @@ function isSafeToPass(array) {
   return 3;
 }
 
-function checkLength(array){
+function checkLength(array) {
   if (array.length > 11 || array.length < 11) {
     return 1;
   }
@@ -208,7 +208,7 @@ function generatePhoneNumber(arrayNumber) {
   }
 
   if (isSafeToPass(arrayNumber) === 3) {
-    return `(${arrayNumber[0]}${arrayNumber[1]}) ${arrayNumber[2]}${arrayNumber[3]}${arrayNumber[4]}${arrayNumber[5]}${arrayNumber[6]}${arrayNumber[7]}${arrayNumber[8]}${arrayNumber[9]}${arrayNumber[10]}`; 
+    return `(${arrayNumber[0]}${arrayNumber[1]}) ${arrayNumber[2]}${arrayNumber[3]}${arrayNumber[4]}${arrayNumber[5]}${arrayNumber[6]}${arrayNumber[7]}${arrayNumber[8]}${arrayNumber[9]}${arrayNumber[10]}`;
 
   }
   return 'Não rolou nada!';
