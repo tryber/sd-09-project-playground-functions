502205c0acb5ddc6bc45134c583f1461541318ba
Felipe Rocha <felipe.sr27@gmail.com>
2020-12-06 23:10:56 -0300

    Correcao de complexidade cognitiva no requisito 11 apontado pelo code climate.

--- src/challenges.js
+++ src/challenges.js
@@ -156,14 +156,9 @@ function generatePhoneNumber(array) {
   let telephone = '';
   if (array.length === 11) {
     for (let index = 0; index < array.length; index += 1) {
-      let cont = 0;
-      for (let index2 = 0; index2 < array.length; index2 += 1) {
-        if (array[index] === array[index2]) {
-          cont += 1;
-          if (cont >= 3) {
-            return 'não é possível gerar um número de telefone com esses valores';
-          }
-        }
+      verifyNumRepetition(array[index], array);
+      if (cont >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
       }
       switch (true) {
         case ((array[index] < 0) || (array[index] > 9)):
@@ -187,6 +182,16 @@ function generatePhoneNumber(array) {
   return 'Array com tamanho incorreto.';
 }
 
+function verifyNumRepetition (number, array) {
+  let cont = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (number === array[index2]) {
+      cont += 1;
+    }
+  }
+  return cont;
+}
+
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   let sumBC = lineB + lineC;
@@ -196,7 +201,7 @@ function triangleCheck(lineA, lineB, lineC) {
   let subAC = Math.abs(lineA - lineC);
   let subAB = Math.abs(lineA - lineB);
   if ((lineA > (sumBC) || lineB > (sumAC) || lineC > (sumAB))) {
-    if ((lineA > (subBC) || lineB > (subAC) || lineC > (subAB))){
+    if ((lineA > (subBC) || lineB > (subAC) || lineC > (subAB))) {
       return false;
     }
   }
