f869d0298fea2e0e4a3fa837a993b4f7ed9365a7
Rafael Pufal <rafael.pufal23@gmail.com>
2020-12-07 11:54:07 -0300

    adicionando requisito 11

--- src/challenges.js
+++ src/challenges.js
@@ -176,11 +176,39 @@ function techList(toLearn, name) {
 // Desafio 11
 function generatePhoneNumber(telNumber) {
   // seu código aqui
+  let finalNumber ="("
   if (telNumber.length !== 11){
     return "Array com tamanho incorreto."
   }
-
+  dicionario = {"0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9":0}
+  for (i=0; i<telNumber.length; i++){
+    if(telNumber[i] < 0 || telNumber[i]>9){
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+    else{
+      dicionario[telNumber[i]] += 1
+      if(i === 2){
+        finalNumber += ")"
+        finalNumber += telNumber[i]
+      }
+      else if(i === 7){
+        finalNumber += "-"
+        finalNumber += telNumber[i]
+      }
+      else{
+        finalNumber += telNumber[i]
+      }
+    }
+  }
+  if (Object.values(dicionario).includes(3)){
+    return "não é possível gerar um número de telefone com esses valores"
+  }
+  else{
+    return finalNumber
+  }
 }
+// let teste11 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
+// console.log(generatePhoneNumber(teste11))
 
 // Desafio 12
 function triangleCheck() {
