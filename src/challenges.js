dad32b7303acb5b044eb4caeb316361a84a34cd1
Felipe Shcolnik <shcolnik@gmail.com.br>
2020-12-07 11:29:36 -0300

    codeclimate

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,11 @@
-//Teste
+// Teste
 // Desafio 1 OK
 function compareTrue(bool1, bool2) {
   if (bool1 === true && bool2 === true) {
     return true;
-  } 
-  return false;
   }
+  return false;
+}
 console.log(compareTrue(true, true));
 
 // Desafio 2 OK
@@ -16,7 +16,7 @@ console.log(calcArea(3, 8));
 
 // Desafio 3
 function splitSentence(string3) {
-  let arrayString3 = string3.split(" ");
+  let arrayString3 = string3.split(' ');
   return arrayString3;
 }
 
@@ -33,22 +33,21 @@ function footballPoints(wins, ties) {
   return result;
 }
 
-//Desafio 6
-let highest = "";
+// Desafio 6
 function highestNumber(array6) {
   let index = 0;
   let highest = array6[index];
-    for (index = 0; index < array6.length; index ++) {
-      if (highest < array6[index]) {
-        highest = array6[index];
-      }
+  for (index = 0; index < array6.length; index=+ 1) {
+    if (highest < array6[index]) {
+      highest = array6[index];
     }
+  }
   return highest;
 }
 function highestCount(array6) {
   let index = 0;
   let counter = 0;
-  for (index = 0; index < array6.length; index ++) {
+  for (index = 0; index < array6.length; index=+1) {
     if (highestNumber(array6) === array6[index]) {
       counter = counter + 1;
     }
@@ -60,27 +59,26 @@ function catAndMouse(mouse, cat1, cat2) {
   let distance1 = Math.abs((cat1 - mouse));
   let distance2 = Math.abs((cat2 - mouse));
   if (distance1 < distance2) {
-    return "cat1";
+    return 'cat1';
   } else if (distance1 > distance2) {
-    return "cat2";
-  } else {
-    return "os gatos trombam e o rato foge";
-  }
+    return 'cat2';
+  } 
+  return 'os gatos trombam e o rato foge';
 }
 console.log(catAndMouse(1, 2, 0));
 
 // Desafio 8
 function fizzBuzz(array8) {
   let index = 0
-  for (index = 0; index < array8.length; index ++) {
-    if (array8[index] % 3 == 0 && array8[index] % 5 == 0) {
+  for (index = 0; index < array8.length; index =+1) {
+    if (array8[index] % 3 === 0 && array8[index] % 5 === 0) {
         array8[index] = "fizzBuzz";
-    } else if (array8[index] % 3 == 0) {
-      array8[index] = "fizz";
-    } else if (array8[index] % 5 == 0) {
-      array8[index] = "buzz";
+    } else if (array8[index] % 3 === 0) {
+      array8[index] = 'fizz';
+    } else if (array8[index] % 5 === 0) {
+      array8[index] = 'buzz';
     } else {
-    array8[index] = "bug!";
+    array8[index] = 'bug!';
     }
   }
   return array8;
@@ -89,17 +87,17 @@ function fizzBuzz(array8) {
 
 // Desafio 9
 function encode(string9) {
-  let object = {"a":"1","e":"2","i":"3","o":"4","u":"5"};
+  let object = { 'a':'1','e':'2','i':'3','o':'4','u':'5'};
   for (let key in object) {
     let substitute = key;
-    let re = new RegExp(substitute,"g");
+    let re = new RegExp(substitute, 'g');
     string9 = string9.replace(re, object[key]);
   }
-return string9;
+  return string9;
 }
 
 function decode(string9) {
-  let object = {"1":"a","2":"e","3":"i","4":"o","5":"u"};
+  let object = { '1':'a','2':'e','3':'i','4':'o','5':'u'};
   for (let key in object) {
     let substitute = key;
     let re = new RegExp(substitute,"g");
@@ -109,8 +107,8 @@ return string9;
 }
 
 // Desafio 10
-let arraytech = ["React", "Jest", "HTML", "CSS", "JavaScript"];
-let name = "Felipe";
+let arraytech = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
+let name = 'Felipe';
 function techList(arraytech, name) {
 // seu código aqui
   let arrayTech = [];
@@ -119,23 +117,36 @@ function techList(arraytech, name) {
     arrayTech.push({
       tech: arraytech[index],
       name,
-    });   
+      }
+    );
   }
   if (arrayTech.length === 0) {
-  return 'Vazio!';
+    return 'Vazio!';
   }
   return arrayTech;
 }
 
 // Desafio 11
 function generatePhoneNumber(arraytelephone) {
-  
+}
+// Desafio 12
+lineA = 15;
+lineB = -12
+lineC = 2
+// let res = (Math.abs(lineB)- Math.abs(lineC));
+//   console.log(res);
+function triangleCheck(lineA, lineB, lineC) {
+  // seu código aqui
+  let result = false;
+  if (lineA*lineB*lineC != 0) {
+    if (lineA > (lineB + lineC)) && (Math.abs(lineA) > (Math.abs(lineB)- Math.abs(lineC)));{
+      result = true;
+    }
+  }
+  return result
+  console.log(result);
 }
 
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
 
 // Desafio 13
 function hydrate() {
