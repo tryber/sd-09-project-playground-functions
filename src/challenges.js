4e5dc45b79f362d0516ddad1edeff2144830ab9e
Osvaldo Borges <netoo70@gmail.com>
2020-12-07 10:33:36 -0300

    Fazendo o CodeClimate

--- src/challenges.js
+++ src/challenges.js
@@ -15,14 +15,14 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(string) {
-  // seu código aqui 
-let array = [];
-let word = '';
+  // seu código aqui
+  let array = [];
+  let word = '';
 
   for (let contador = 0; contador < string.length; contador += 1) {
-    if (string[contador] != ' ') {
+    if (string[contador] !== ' ') {
       word += string[contador];
-    }else {
+    } else {
       array.push(word);
       word = '';
     }
@@ -35,53 +35,53 @@ let word = '';
 function concatName(array) {
   // seu código aqui
 
- let string = '';
+  function primeiroItem(array1) {
+    let primeiroItem = '';
+    primeiroItem += array1[array1.length - 1];
+    return primeiroItem;
+  }
 
- function primeiroItem(array){
-   let primeiroItem = '';
-   primeiroItem += array[array.length - 1];
-   return primeiroItem;
- }
- 
- function segundoItem(array){
-   let segundoItem = '';
-   segundoItem += array[0];
-   return segundoItem;
- }
- 
- string = (primeiroItem(array) + ', ' + segundoItem(array));
- 
- return string;
- }
+  function segundoItem(array2) {
+    let segundoItem = '';
+    segundoItem += array2[0];
+    return segundoItem;
+  }
+
+  let string = '';
+
+  string = (primeiroItem(array) + ', ' + segundoItem(array));
+
+  return string;
+}
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
-let pontos = 0;
+  let pontos = 0;
 
-function pontosWins(wins) {
-  let pontosWins = 0;
-  pontosWins = wins * 3;
-  return pontosWins;
-}
-  
-function pontosTies(ties) {
-  let pontosTies = 0;
-  pontosTies = ties * 1;
-  return pontosTies;
-}
+  function pontosWins(wins) {
+    let pontosWins = 0;
+    pontosWins = wins * 3;
+    return pontosWins;
+  }
 
-pontos = pontosWins(wins) + pontosTies(ties);
-  
-return pontos;
+  function pontosTies(ties) {
+    let pontosTies = 0;
+    pontosTies = ties * 1;
+    return pontosTies;
+  }
+
+  pontos = pontosWins(wins) + pontosTies(ties);
+
+  return pontos;
 }
 
 // Desafio 6
 function highestCount(array) {
   // seu código aqui
 
-let somaNumero = 0;
-let maiorNumero = 0;
+  let somaNumero = 0;
+  let maiorNumero = 0;
 
   for (let contador = 0; contador < array.length; contador += 1) {
     if (array[contador] > maiorNumero) {
@@ -100,9 +100,9 @@ let maiorNumero = 0;
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
 
-function catFunct1(mouse, cat1) {
+  function catFunct1(mouse, cat1) {
 
-let distancia1 = 0;
+  let distancia1 = 0;
 
   if (mouse < cat1){
     distancia1 = cat1 - mouse;
@@ -110,11 +110,11 @@ let distancia1 = 0;
     distancia1 = mouse - cat1;
   }
   return distancia1;
-}
+  }
 
-function catFunct2(mouse, cat2) {
+  function catFunct2(mouse, cat2) {
 
-let distancia2 = 0;
+  let distancia2 = 0;
 
   if (mouse < cat2){
     distancia2 = cat2 - mouse;
@@ -122,7 +122,7 @@ let distancia2 = 0;
     distancia2 = mouse - cat2;
   }
   return distancia2;
-}
+  }
 
   if (catFunct1(mouse, cat1) < catFunct2(mouse, cat2)) {
     return 'cat1';
@@ -137,8 +137,8 @@ let distancia2 = 0;
 function fizzBuzz(array) {
   // seu código aqui
 
-let retornoArray = [];
-  
+  let retornoArray = [];
+
   for (let contador = 0; contador < array.length; contador += 1) {
     if (array[contador] % 5 === 0 && array[contador] % 3 === 0) {
       retornoArray.push('fizzBuzz');
