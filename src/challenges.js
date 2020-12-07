6ae5a66ea1124805d08a75661005bedfda4cd6da
Felipe Shcolnik <shcolnik@gmail.com.br>
2020-12-07 11:56:32 -0300

    codeclimate2

--- src/challenges.js
+++ src/challenges.js
@@ -128,23 +128,38 @@ function techList(arraytech, name) {
 
 // Desafio 11
 function generatePhoneNumber(arraytelephone) {
+//   if (arraytelephone.length !== 10) {
+//     console.log('Array com tamanho incorreto.');
+//   } else {
+//     for (let index=0; index < arraytelephone.length; index =+1){
+//       if (arraytelephone[index] < 0 || arraytelephone[index] < 9){
+//         console.log('tem negativo ou maior q 10');
+//       }
+//     }
+//     for (let index=0; index < 8; index =+1){
+//       if (arraytelephone[index] === arraytelephone[index+1] === arraytelephone[index+2]) {
+//         console.log('não é possível gerar um número de telefone com esses valores');
+//       }
+//   }
+//   return
 }
+// function generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7]);
 // Desafio 12
-lineA = 15;
-lineB = -12
-lineC = 2
+// lineA = 15;
+// lineB = -12
+// lineC = 2
 // let res = (Math.abs(lineB)- Math.abs(lineC));
 //   console.log(res);
 function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
-  let result = false;
-  if (lineA*lineB*lineC != 0) {
-    if (lineA > (lineB + lineC)) && (Math.abs(lineA) > (Math.abs(lineB)- Math.abs(lineC)));{
-      result = true;
-    }
-  }
-  return result
-  console.log(result);
+  // let result = false;
+  // if (lineA*lineB*lineC != 0) {
+  //   if (lineA > (lineB + lineC)) && (Math.abs(lineA) > (Math.abs(lineB)- Math.abs(lineC)));{
+  //     result = true;
+  //   }
+  // }
+  // return result
+  // console.log(result);
 }
 
 
