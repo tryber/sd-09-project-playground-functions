ef09d78718b426607eb97daa08805bf05af6781e
Felipe Shcolnik <shcolnik@gmail.com.br>
2020-12-08 00:02:41 -0300

    codeclimate

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1 OK
 function compareTrue(bool1, bool2) {
-  return bool1&&bool2
+  return bool1 && bool2
 }
 
 // Desafio 2 OK
@@ -23,7 +23,7 @@ function concatName(frase4) {
 // Desafio 5
 function footballPoints(wins, ties) {
   const winpoint = 3;
-  let result = (wins*winpoint)+ties;
+  let result = (wins * winpoint) + ties;
   return result;
 }
 
@@ -44,7 +44,7 @@ function highestCount(array6) {
   let counter = 0;
   for (index = 0; index < array6.length; index += 1) {
     if (highestNumber(array6) === array6[index]) {
-      counter = counter + 1;
+      counter = counter += 1;
     }
   }
   return counter;
@@ -57,7 +57,7 @@ function catAndMouse(mouse, cat1, cat2) {
     return 'cat1';
   } else if (distance1 > distance2) {
     return 'cat2';
-  } 
+  }
   return 'os gatos trombam e o rato foge';
 }
 
@@ -66,13 +66,13 @@ function fizzBuzz(array8) {
   let index = 0
   for (index = 0; index < array8.length; index += 1) {
     if (array8[index] % 3 === 0 && array8[index] % 5 === 0) {
-        array8[index] = "fizzBuzz";
+      array8[index] = 'fizzBuzz';
     } else if (array8[index] % 3 === 0) {
       array8[index] = 'fizz';
     } else if (array8[index] % 5 === 0) {
       array8[index] = 'buzz';
     } else {
-    array8[index] = 'bug!';
+      array8[index] = 'bug!';
     }
   }
   return array8;
@@ -80,7 +80,7 @@ function fizzBuzz(array8) {
 
 // Desafio 9
 function encode(string9) {
-  let object = { 'a':'1','e':'2','i':'3','o':'4','u':'5'};
+  let object = { 'a':'1', 'e':'2', 'i':'3', 'o':'4', 'u':'5'};
   for (let key in object) {
     let substitute = key;
     let re = new RegExp(substitute, 'g');
@@ -89,18 +89,16 @@ function encode(string9) {
   return string9;
 }
 function decode(string9) {
-  let object = { '1':'a','2':'e','3':'i','4':'o','5':'u'};
-  for (let key in object) {
-    let substitute = key;
-    let re = new RegExp(substitute,"g");
-    string9 = string9.replace(re, object[key]);
+  let object2 = { '1':'a', '2':'e', '3':'i', '4':'o', '5':'u'};
+  for (let key2 in object2) {
+    let substitute2 = key2;
+    let re2 = new RegExp(substitute2, 'g');
+    string92 = string92.replace(re2, object2[key2]);
   }
-return string9;
+  return string92;
 }
 
 // Desafio 10
-let arraytech = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
-let name = 'Felipe';
 function techList(arraytech, name) {
 // seu código aqui
   let arrayTech = [];
@@ -109,7 +107,7 @@ function techList(arraytech, name) {
     arrayTech.push({
       tech: arraytech[index],
       name,
-      }
+    }
     );
   }
   if (arrayTech.length === 0) {
