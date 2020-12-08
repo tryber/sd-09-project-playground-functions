c026e8143050f3370a7dc6c0847a4b9497f86338
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2020-12-08 18:34:06 -0300

    refatoracao do projeto

--- src/challenges.js
+++ src/challenges.js
@@ -33,7 +33,6 @@ function highestCount(numbers) {
   for (let i = 1; i < numbers.length; i += 1) {
     if (greaterNumber < numbers[i]) {
       greaterNumber = numbers[i];
-      counter = 1;
     } else if (greaterNumber === numbers[i]) {
       counter += 1;
     }
