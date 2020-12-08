f8a4daa72506324128ced4f0051712d5d95833c9
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-08 14:17:15 -0300

    working on climate 7

--- src/challenges.js
+++ src/challenges.js
@@ -175,9 +175,9 @@ function generatePhoneNumber(numbers) {
   for (let index = 0; index < numbers.length; index += 1) {
     rN.push(numbers[index]);
   }
-  return `(${rN[0]}${rN[1]}) ${rN[2]}${rN[3]}${rN[4]}${rN[5]}${rN[6]} ${rN[7]}${rN[8]}${rN[9]}${rN[10]}`
+  return `(${rN[0]}${rN[1]}) ${rN[2]}${rN[3]}${rN[4]}${rN[5]}${rN[6]} - ${rN[7]}${rN[8]}${rN[9]}${rN[10]}`
 }
-console.log(generatePhoneNumber([8, 9, 3, 4, 5, 6, 2, 9, 7, 7, 0]));
+console.log(generatePhoneNumber([8, 9, 3, 4, 5, 6, 2, 9, 5, 7, 0]));
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -200,7 +200,7 @@ function hydrate(waterBottle) {
     soma += Number(matches[index]);
   }
   if (soma === 1) {
-    return `${soma} copo de água'`;
+    return `${soma} copo de água`;
   }
   return `${soma} copos de água`;  
 }
