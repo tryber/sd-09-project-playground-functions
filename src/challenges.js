bf700a06f1c25070adca5b058d792fbbc947d533
thosijulio <thosijulio@gmail.com>
2020-12-08 01:16:05 -0300

    Correcoes codeClimate

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,9 @@
 /* Projeto: Playground Functions;
    Aluno: Julio Henrique Thosi;
    Turma: 09; */
-   
 // Desafio 1
-function compareTrue(vd1_1,vd1_2) {
-  if(vd1_1 === true && vd1_2 === true){
+function compareTrue(vD1_1,vD1_2) {
+  if (vD1_1 === true && vD1_2 === true) {
     return true;
   } else {
     return false;
@@ -12,38 +11,38 @@ function compareTrue(vd1_1,vd1_2) {
 }
 
 // Desafio 2
-function calcArea(base,height) {
-  area = (base*height)/2;
+function calcArea(base, height) {
+  let area = (base * height)/2;
   return area;
 }
 
 // Desafio 3
-function splitSentence(vd3) {
-  return vd3.split(" ");
+function splitSentence(vD3) {
+  return vD3.split('');
 }
 
 // Desafio 4
-function concatName(vd4) {
-  return vd4[vd4.length-1] + ', ' + vd4[0];
+function concatName(vD4) {
+  return vD4[vD4.length-1] + ', ' + vD4[0];
 }
 
 // Desafio 5
-function footballPoints(wins,ties) {
-  let teamPoints = (wins*3) + ties;
+function footballPoints(wins, ties) {
+  let teamPoints = (wins * 3) + ties;
   return teamPoints;
 }
 
 // Desafio 6
-function highestCount(vd6) {
+function highestCount(countRepetition) {
   let maior = 0;
   let qtdeMaior = 0;
-  for(let index = 0; index < 8; index += 1){
-    if(maior < vd6[index]){
-      maior = vd6[index];
+  for (let index = 0; index < 8; index += 1) {
+    if(maior < countRepetition[index]) {
+      maior = countRepetition[index];
     }
   }
-  for(index = 0; index < 8; index += 1){
-    if(maior === vd6[index]){
+  for (index = 0; index < 8; index += 1) {
+    if (maior === countRepetition[index]) {
       qtdeMaior += 1;
     }
   }
@@ -52,18 +51,18 @@ function highestCount(vd6) {
 
 // Desafio 7
 function catAndMouse(mouse,cat1,cat2) {
-  
+
   let distIgual = 'os gatos trombam e o rato foge';
   let cat1s = Math.abs (cat1 - mouse);
   let cat2s = Math.abs (cat2 - mouse);
   let phrase = '';
 
-  if(cat1s < cat2s){
+  if (cat1s < cat2s) {
   phrase = 'cat1';
   return phrase;
   }
-  
-  else if (cat1s > cat2s){
+
+  else if (cat1s > cat2s) {
   phrase = 'cat2';
   return phrase;
   }
@@ -75,11 +74,11 @@ function catAndMouse(mouse,cat1,cat2) {
 
 // Desafio 8
 function fizzBuzz(vd8) {
-  
+
   let result = [];
 
-  for(let index = 0; index < vd8.length; index += 1){
-    
+  for (let index = 0; index < vd8.length; index += 1) {
+
     if ((vd8[index] % 3) === 0 && (vd8[index] % 5) === 0)
     result.push ('fizzBuzz');
     else if((vd8[index] % 3) === 0)
@@ -137,4 +136,3 @@ module.exports = {
   splitSentence,
   triangleCheck,
 }
-
