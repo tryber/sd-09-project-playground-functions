4ee547327a00d11ac3c961c7927cda1467e1f013
Emanoel Leão <emanuelleaomarinho@gmail.com>
2020-12-08 00:49:37 -0300

    Desafio 13: Melhorando o código segundo o code climate

--- src/challenges.js
+++ src/challenges.js
@@ -264,10 +264,13 @@ function hydrate(string) {
   let regex = /\d/g;
   let sum = 0;
   let match;
-
-  while (match = regex.exec(string)) {
+  do {
     sum += parseInt(match[0], 10);
-  }
+  } while (match = regex.exec(string))
+
+  // while (match = regex.exec(string)) {
+  //   sum += parseInt(match[0], 10);
+  // }
   if (sum < 2) {
     return `${sum} copo de água`;
   }
