9ece615bf37f861f162b2edc4c096c316a000cae
fernandacajueiro <fernandacajueiromachado@gmail.com>
2020-12-07 23:02:57 -0300

    Adiciona desafio 12

--- src/challenges.js
+++ src/challenges.js
@@ -194,9 +194,16 @@ function generatePhoneNumber(array) {
 // console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA, lineB, lineC) {
+  let isTriangle = false;
+  if ((Math.abs(lineB - lineC) < lineA) && (lineA < (lineB+lineC)) &&
+      (Math.abs(lineA - lineC) < lineB) && (lineB < (lineA+lineC)) &&
+      (Math.abs(lineA - lineB) < lineC) && (lineC < (lineA+lineC))) {
+    isTriangle = true;
+    }
+  return isTriangle;
+  }
+ // console.log(triangleCheck(10, 14, 8));
 
 // Desafio 13
 function hydrate() {
