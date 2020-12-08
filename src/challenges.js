5f6bd79cb10c2e966be0aa8f10c1276c8a560ffd
Raphael Rêgo <raphael.mlrego@gmail.com>
2020-12-08 13:00:43 -0300

    desafio 12

--- src/challenges.js
+++ src/challenges.js
@@ -141,8 +141,12 @@ function generatePhoneNumber(arrayPhoneNumber) {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC) && lineB < (lineA + lineC) && lineB > Math.abs(lineA - lineC) && lineC < (lineA + lineB) && lineC < Math.abs(lineA + lineB)) {
+    return true
+  } else {
+    return false
+  }
 }
 
 // Desafio 13
