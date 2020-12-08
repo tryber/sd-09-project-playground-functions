96756fcfa3c520893421f6d4aa1b25b9520b23f3
leonardo <rpgleo.js@gmail.com>
2020-12-08 17:44:40 -0300

    11 exercise - commit 2

--- src/challenges.js
+++ src/challenges.js
@@ -206,7 +206,7 @@ function generatePhoneNumber(numbers) {
         return 'não é possível gerar um número de telefone com esses valores';
       }
     }
-    result = `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]} `;
+    result = `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
   }
   return result;
   // seu código aqui
