5dbb3f14167ad500e0df934fcb06fa9b3c4269a1
Renan Costa <renancosta21@gmail.com>
2020-12-10 16:10:05 -0300

    exercício 8

--- src/challenges.js
+++ src/challenges.js
@@ -57,8 +57,20 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let arrayResult = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] % 3 === 0 && array[index] % 5 !== 0) {
+      arrayResult.push('fizz');
+    } else if (array[index] % 3 !== 0 && array[index] % 5 === 0) {
+      arrayResult.push('buzz');
+    } else if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+      arrayResult.push('fizzBuzz');
+    } else if (array[index] % 3 !== 0 && array[index] % 5 !== 0) {
+      arrayResult.push('bug!');
+    }
+  }
+  return arrayResult;
 }
 
 // Desafio 9
