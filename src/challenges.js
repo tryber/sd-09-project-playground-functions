3ad485b14c87f8eb480641e7dc321e634040bdc5
mvmes2 <mvmes23@gmail.com>
2020-12-11 12:00:45 -0300

    desafio 11 finalizado, tentativa 4, desisti do Climate Again, agora acho que vai... senão é sacanagem já =x?

--- src/challenges.js
+++ src/challenges.js
@@ -173,7 +173,7 @@ function validatePossibleNumber(array, trigger) {
 }
 function generatingNumber(array) {
   let formater = '';
-  formater = '(' + array[0] + array[1] + ')' + ' ' + array[2] + array[3] + array[4] + array[5] + '-' + array[6] + array[7] + array[8] + array[9] + array[10];
+  formater = '(' + array[0] + array[1] + ')' + ' ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
   return formater;
 }
 function generatePhoneNumber(array) {
