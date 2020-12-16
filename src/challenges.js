a3f92f11d9de27119afcc4f7cf1f754a9930af8a
Roberto Natividade <robertonnv@gmail.com>
2020-12-16 17:23:39 -0300

    Codigo do exerciocio 5

--- src/challenges.js
+++ src/challenges.js
@@ -24,25 +24,14 @@ function concatName(sArray) {
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
-  if(wins === true){
-    for (let index = 0; index <= wins.length; index = index + 1){
-      
-      let contWins = contWins + index;
       let pointsWins = 3;
-      let totalPointsWins = (contWins * pointsWins);
-      index = index + 1;
-    }
-  }
-  if(ties === true){
-    for (index2 = 0; index2 <= ties.length; index2 +=1){
-      let contTies = contTies + index2
+      let totalPointsWins = (Wins * pointsWins);
       let pointsTies = 1;
-      let totalPointsTies = (contTies * pointsTies);
-      index2 = index2 + 1;
+      let totalPointsTies = (ties * pointsTies);
+      return (totalPointsWins + totalPointsTies);
     } 
-  }
-  return (totalPointsWins + totalPointsTies);
-}
+  
+
 // Desafio 6
 function highestCount(matrix) {
   let number = matrix[0];
