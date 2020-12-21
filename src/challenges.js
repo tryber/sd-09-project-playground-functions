682f47d1bde9f2f5d89fe0de8df8700165a35a94
Pablo Iago <evangelista.pabloiago@gmail.com>
2020-12-21 17:45:16 -0300

    Desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -9,8 +9,8 @@ function compareTrue(number1, number2) {
 
 // Desafio 2
 function calcArea(base, height) {
-   let resultado = (base * height)/2
-   return resultado
+  let resultado = (base * height)/2
+  return resultado
 }
 
 // Desafio 3
