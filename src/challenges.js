f23d95d1e9a4831ecff14e6c0afaf7e209540640
Pablo Iago <evangelista.pabloiago@gmail.com>
2020-12-22 15:04:03 -0300

    Desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -48,38 +48,69 @@ function highestCount(array) {
 function catAndMouse(mouse, cat1, cat2) {
   let distancia1 = Math.abs(mouse - cat1)
   let distancia2 = Math.abs(mouse - cat2)
-    if (distancia1 === distancia2) {
-        return 'os gatos trombam e o rato foge';
-    } else if (distancia1 < distancia2) {
-        return 'cat1';
-    } else if (distancia2 < distancia1) {
-        return 'cat2';
-    }
+  if (distancia1 === distancia2) {
+      return 'os gatos trombam e o rato foge';
+  } else if (distancia1 < distancia2) {
+      return 'cat1';
+  } else if (distancia2 < distancia1) {
+      return 'cat2';
+  }
 }
 
 // Desafio 8
 function fizzBuzz(array) {
   let fizzBuzz = array;
-    for (let i = 0; pos < fizzBuzz.length; i += 1)
-        if (fizzBuzz[i] % 3 === 0 && fizzBuzz[pos] % 5 === 0) {
-            fizzBuzz[i] = "fizzBuzz";
-        } else if (fizzBuzz[i] % 3 == 0) {
-        fizzBuzz[i] = "fizz";
-    } else if (fizzBuzz[i] % 5 == 0) {
-        fizzBuzz[i] = "buzz";
+  for (let i = 0; pos < fizzBuzz.length; i += 1){
+    if (fizzBuzz[i] % 3 === 0 && fizzBuzz[i] % 5 === 0) {
+      fizzBuzz[i] = "fizzBuzz";
+    } else if (fizzBuzz[i] % 3 === 0) {
+      fizzBuzz[i] = "fizz";
+    } else if (fizzBuzz[i] % 5 === 0) {
+      fizzBuzz[i] = "buzz";
     } else {
-        fizzBuzz[i] = "bug!";
+      fizzBuzz[i] = "bug!";
     }
-
-    return fizzBuzz;
+  }
+  return fizzBuzz;
 }
 
 // Desafio 9
 function encode(string) {
-  // seu código aqui
+  let cripted = "";
+    for (let i = 0; i < string.length; i += 1) {
+        if (string[i] === "a") {
+            cripted += "1";
+        } else if (string[i] === "e") {
+            cripted += "2";
+        } else if (string[i] === "i") {
+            cripted += "3";
+        } else if (string[i] === "o") {
+            cripted += "4";
+        } else if (string[i] === "u") {
+            cripted += "5";
+        } else {
+            cripted += string[i];
+        }
+    }
+    return cripted;
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  let uncripted = "";
+    for (let i = 0; i < string.length; i += 1)
+        if (string[i] === "1") {
+        uncripted += "a";
+    } else if (string[i] === "2") {
+        uncripted += "e";
+    } else if (string[i] === "3") {
+        uncripted += "i";
+    } else if (string[i] === "4") {
+        uncripted += "o";
+    } else if (string[i] === "5") {
+        uncripted += "u";
+    } else {
+        uncripted += string[i];
+    }
+    return uncripted;
 }
 
 // Desafio 10
